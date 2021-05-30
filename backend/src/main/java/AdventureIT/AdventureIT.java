6ff4e98bf8df9f2391b74928cb94012dc3089529
Alex <u19069716@tuks.co.za>
2021-05-30 16:49:54 +0200

    build: springboot project set up

---
+++ backend/src/main/java/AdventureIT/AdventureIT.java
@@ -0,0 +1,13 @@
+package AdventureIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureIT {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureIT.class, args);
+	}
+
+}
