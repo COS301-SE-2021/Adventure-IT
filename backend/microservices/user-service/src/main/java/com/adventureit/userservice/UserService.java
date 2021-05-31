5dc2707ed33e5312535601932d721206ffc4eee0
Alex <u19069716@tuks.co.za>
2021-05-31 11:41:54 +0200

    refactor(User): adherence to microservices

---
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/UserService.java
@@ -0,0 +1,4 @@
+package com.adventureit.userservice;
+
+public interface UserService {
+}
---
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/UserServiceImplementation.java
@@ -0,0 +1,7 @@
+package com.adventureit.userservice;
+
+import org.springframework.stereotype.Service;
+
+@Service("UserServiceImplementation")
+public class UserServiceImplementation implements UserService {
+}
