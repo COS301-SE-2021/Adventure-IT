b3e0ba76b8256ff0506c3ccf40a3ea5fc46c7770
Alex <u19069716@tuks.co.za>
2021-05-31 13:39:12 +0200

    build(microservices): added the remainder of the microservices
    added the following microservices
    - budget
    - location
    - media
    - notification
    - sharing

---
+++ backend/microservices/media-service/src/test/java/com/adventureit/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventureit.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
