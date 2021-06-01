2cfdafda5ca4a20a4c83b01a852131da872a83ff
kevichi <kevin9716cui@gmail.com>
2021-06-02 01:33:10 +0200

    feat(GetUserByUUID): Added a getUserByUUID service for deoming as well as response and request objects;

---
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Requests/GetUserByUUIDRequest.java
@@ -0,0 +1,21 @@
+package com.adventureit.userservice.Requests;
+
+import java.util.UUID;
+
+public class GetUserByUUIDRequest {
+    private UUID userID;
+
+    public GetUserByUUIDRequest(UUID userID) {
+        this.userID = userID;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+
+}
--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/Service/UserService.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Service/UserService.java
@@ -4,13 +4,11 @@ import com.adventureit.userservice.Exceptions.InvalidRequestException;
 import com.adventureit.userservice.Exceptions.InvalidUserEmailException;
 import com.adventureit.userservice.Exceptions.InvalidUserPasswordException;
 import com.adventureit.userservice.Exceptions.InvalidUserPhoneNumberException;
-
 import com.adventureit.userservice.Requests.RegisterUserRequest;
-
 import com.adventureit.userservice.Responses.RegisterUserResponse;
 
 public interface UserService {
-    public RegisterUserResponse RegisterUser(RegisterUserRequest req) throws InvalidUserEmailException, InvalidUserPasswordException, InvalidUserPhoneNumberException, InvalidRequestException;
+    RegisterUserResponse RegisterUser(RegisterUserRequest req) throws InvalidUserEmailException, InvalidUserPasswordException, InvalidUserPhoneNumberException, InvalidRequestException;
 
 
 }
