8b99c689bb9fa5506c09e491e084d06e8f37c1d2
simrxn_r <simrathilal@gmail.com>
2021-06-02 01:27:04 +0200

    commented AddUserToAdventure implementation added

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureServiceImplementation.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureServiceImplementation.java
@@ -4,12 +4,12 @@ import com.adventureit.adventureservice.Requests.AddUserToAdventureRequest;
 import com.adventureit.adventureservice.Requests.CreateAdventureRequest;
 import com.adventureit.adventureservice.Responses.AddUserToAdventureResponse;
 import com.adventureit.adventureservice.Responses.CreateAdventureResponse;
-import com.adventureit.userservice.Service.UserServiceImplementation;
+//import com.adventureit.userservice.Service.UserServiceImplementation;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 public class AdventureServiceImplementation implements AdventureService{
-    private UserServiceImplementation user;
+    //private UserServiceImplementation user;
 
     @Override
     public CreateAdventureResponse createAdventure(CreateAdventureRequest req) {
@@ -19,6 +19,16 @@ public class AdventureServiceImplementation implements AdventureService{
     @Override
     public AddUserToAdventureResponse AddUserToAdventure(AddUserToAdventureRequest req)
     {
+        /*UUID userID = req.getUserID();
+        UUID advID = req.getAdventureID();
+        GetUserByUUIDRequest request = new GetUserByUUIDRequest(userID);
+        GetUserByUUIDResponse res = user.GetUserByUUID(request);
+        User userToBeAdded = null;
+        if(res.isSuccess())
+        {   userToBeAdded = res.getUser();  }
+
+        return new AddUserToAdventureResponse(true, userToBeAdded.getFirstname()+" "+userToBeAdded.getLastname+" has been added to adventure: ");
+        */
         return new AddUserToAdventureResponse(true, " has been added to adventure: ");
     }
 
