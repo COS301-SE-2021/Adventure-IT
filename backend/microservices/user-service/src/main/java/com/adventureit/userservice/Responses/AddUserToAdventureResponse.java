ce87cf3e4ffab4cecf12dcfc40027a1fa31a07fd
simrxn_r <simrathilal@gmail.com>
2021-06-01 17:23:56 +0200

    AddUserToAdventureResponse constructor created

--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/Responses/AddUserToAdventureResponse.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Responses/AddUserToAdventureResponse.java
@@ -1,4 +1,14 @@
 package com.adventureit.userservice.Responses;
 
 public class AddUserToAdventureResponse {
+    private boolean success;
+    private String message;
+    private String token;
+
+    public AddUserToAdventureResponse(boolean success,String token, String message)
+    {
+        this.success = success;
+        this.message = message;
+        this.token =token;
+    }
 }
