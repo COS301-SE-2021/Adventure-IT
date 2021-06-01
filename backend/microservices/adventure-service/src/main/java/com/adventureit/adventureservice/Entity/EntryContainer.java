94504ef6fd49ab1678ae663191fc043862234c0c
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-01 15:11:01 +0200

    Added models for entryContainer, entry, Itinerary and ItineraryEntry
    Models added with getter, setters and basic attributes

---
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Entity/EntryContainer.java
@@ -0,0 +1,26 @@
+package com.adventureit.adventureservice.Entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class EntryContainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private UUID creatorID;
+    private UUID adventureID;
+
+    // Default constructor
+    public EntryContainer(){}
+
+    public UUID getCreatorID(){return this.creatorID;}
+
+    public UUID getAdventureID(){return this.adventureID;}
+
+    public void setCreatorID(UUID cID){this.creatorID=cID;}
+
+    public void setAdventureID(UUID aID){this.adventureID=aID;}
+}
