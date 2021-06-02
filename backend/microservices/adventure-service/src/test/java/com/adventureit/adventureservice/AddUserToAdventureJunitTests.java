1d6e9b59ac674b98fa4243dfcba7cc1387ea57cc
simrxn_r <simrathilal@gmail.com>
2021-06-02 02:54:28 +0200

    AddUserToAdventureRequest JUnit test

--- backend/microservices/adventure-service/src/test/java/com/adventureit/adventureservice/AddUserToAdventureJunitTests.java
+++ backend/microservices/adventure-service/src/test/java/com/adventureit/adventureservice/AddUserToAdventureJunitTests.java
@@ -1,9 +1,25 @@
 package com.adventureit.adventureservice;
 
-import java.util.Random;
+import com.adventureit.adventureservice.Requests.AddUserToAdventureRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Description;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AddUserToAdventureJunitTests {
-    String mockAdventure = "Adventure 1";
     UUID userID = UUID.fromString("933c0a14-a837-4789-991a-15006778f465");
+    UUID advID = UUID.fromString("933c0a14-a837-4789-991a-15006778f123");
+    //test request, response + service
+
+    @Test
+    @Description("This test tests whether the correct information will be retrieved through the request object")
+    void TestAddUserToAdventureRequest()
+    {
+        AddUserToAdventureRequest req = new AddUserToAdventureRequest(userID, advID);
+        assertNotNull(req);
+        assertEquals(userID, req.getUserID());
+        assertEquals(advID, req.getAdventureID());
+    }
 }
