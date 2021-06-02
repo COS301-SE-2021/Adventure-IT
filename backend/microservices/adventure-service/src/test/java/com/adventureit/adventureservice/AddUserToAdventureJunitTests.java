c2710a5d09bc34cb4ed95c22c59de0bc713271ec
simrxn_r <simrathilal@gmail.com>
2021-06-02 23:54:38 +0200

    added commented documentation to AddUserToAdventure unit tests

--- backend/microservices/adventure-service/src/test/java/com/adventureit/adventureservice/AddUserToAdventureJunitTests.java
+++ backend/microservices/adventure-service/src/test/java/com/adventureit/adventureservice/AddUserToAdventureJunitTests.java
@@ -14,7 +14,15 @@ public class AddUserToAdventureJunitTests {
     private AdventureServiceImplementation adventure = new AdventureServiceImplementation();
     UUID userID = UUID.fromString("933c0a14-a837-4789-991a-15006778f465");
     UUID advID = UUID.fromString("933c0a14-a837-4789-991a-15006778f123");
-    //test request, response + service
+
+    /**
+     * Generate mock data to handle JUnit testing
+     *
+     * userID: mock user ID for testing
+     * advID: mock adventure ID for testing
+     *
+     * adventure: mock adventure object
+     */
 
     @Test
     @Description("This test tests whether the correct information will be retrieved through the request object")
