9c1e2331fffb2823ed22b236eeea4480e32b79d1
kevichi <kevin9716cui@gmail.com>
2021-06-02 23:39:59 +0200

    feat(mockController): Created mock controller for UserService

--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/Controller/UserController.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Controller/UserController.java
@@ -1,15 +1,31 @@
 package com.adventureit.userservice.Controller;
 
+import com.adventureit.userservice.Exceptions.InvalidRequestException;
+import com.adventureit.userservice.Exceptions.InvalidUserEmailException;
+import com.adventureit.userservice.Exceptions.InvalidUserPasswordException;
+import com.adventureit.userservice.Exceptions.InvalidUserPhoneNumberException;
+import com.adventureit.userservice.Requests.RegisterUserRequest;
+import com.adventureit.userservice.Responses.RegisterUserResponse;
 import com.adventureit.userservice.Service.UserServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /** This class implements the functionality of the UserAPI interface.*/
 @CrossOrigin("*")
 @RestController
 public class UserController {
 
+
+    private UserServiceImplementation service;
+
     @Autowired
-    private UserServiceImplementation userService;
+    public UserController(UserServiceImplementation service){
+        this.service = service;
+    }
+
+
+    @PostMapping(value = "api/RegisterUser", consumes = "application/json", produces = "application/json")
+    public RegisterUserResponse RegisterUser(@RequestBody RegisterUserRequest req) throws InvalidUserEmailException, InvalidUserPhoneNumberException, InvalidUserPasswordException, InvalidRequestException {
+        return service.RegisterUser(req);
+    }
 }
