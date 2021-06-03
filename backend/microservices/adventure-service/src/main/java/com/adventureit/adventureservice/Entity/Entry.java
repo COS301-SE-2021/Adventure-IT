9768d21f5dc3ce5308dabb90ea8d555dd77122cc
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-03 08:53:36 +0200

    Comments added to implementation for itineraryServiceImplementation

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Entity/Entry.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Entity/Entry.java
@@ -5,6 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.UUID;
+/**
+ *
+ * The params taken in by constructor and what they represent:
+ * 1. long id: The id of the entry to be added to the EntryContainer
+ * 2. long entryContainerID: The id of the entryContainer that the entry is part of
+ *
+ */
 
 @Entity
 public class Entry {
