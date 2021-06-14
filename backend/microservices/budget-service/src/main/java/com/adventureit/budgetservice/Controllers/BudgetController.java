3d3eda16d2cd3b15866c1cc3a5267ae3a309e0b1
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-14 10:52:14 +0200

    Added controller for budget

--- backend/microservices/budget-service/src/main/java/com/adventureit/budgetservice/Controllers/BudgetController.java
+++ backend/microservices/budget-service/src/main/java/com/adventureit/budgetservice/Controllers/BudgetController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 	@GetMapping("/test")
 	String test(){
-		return "Budget Controller is functional";
+		return "Budget Controller is functioning";
 	}
 }
