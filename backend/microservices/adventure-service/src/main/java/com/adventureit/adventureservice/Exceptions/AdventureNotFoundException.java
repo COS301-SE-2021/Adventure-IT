35d859c5b1fc18f75060eabe1c7ff5cc238fb554
Alex <u19069716@tuks.co.za>
2021-06-16 15:17:05 +0200

    feat(adventure-service): mock populate & getAllAdventures
    - populate: Used for populating the persistence layer with mock data
    - getAllAdventures: used for retrieving all adventures

---
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Exceptions/AdventureNotFoundException.java
@@ -0,0 +1,8 @@
+package com.adventureit.adventureservice.Exceptions;
+
+public class AdventureNotFoundException extends Exception {
+
+    public AdventureNotFoundException(String message) {
+        super(message);
+    }
+}
--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureService.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureService.java
@@ -4,10 +4,13 @@ import com.adventureit.adventureservice.Requests.CreateAdventureRequest;
 import com.adventureit.adventureservice.Requests.GetAdventureByUUIDRequest;
 import com.adventureit.adventureservice.Responses.CreateAdventureResponse;
 import com.adventureit.adventureservice.Responses.GetAdventureByUUIDResponse;
+import com.adventureit.adventureservice.Responses.GetAllAdventuresResponse;
 
 public interface AdventureService{
 
     public CreateAdventureResponse createAdventure(CreateAdventureRequest req) throws Exception;
-    public GetAdventureByUUIDResponse getAdventureByUUID (GetAdventureByUUIDRequest req);
+    public GetAdventureByUUIDResponse getAdventureByUUID (GetAdventureByUUIDRequest req) throws Exception;
+    public GetAllAdventuresResponse getAllAdventures() throws Exception;
+    public void mockPopulate();
 //    public AddUserToAdventureResponse AddUserToAdventure(AddUserToAdventureRequest req);
 }
