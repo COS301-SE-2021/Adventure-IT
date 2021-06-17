cd1be3d7b00565b1dbc9080c36687c885f9ef66c
kevichi <kevin9716cui@gmail.com>
2021-06-17 15:42:57 +0200

    feat(createNotificationReq and Response): implemented CreateNotification request and response objects

---
+++ backend/microservices/notification-service/src/main/java/com/adventureit/notificationservice/Requests/CreateNotificationRequest.java
@@ -0,0 +1,22 @@
+package com.adventureit.notificationservice.Requests;
+
+import java.util.UUID;
+
+public class CreateNotificationRequest {
+    private final UUID userId;
+    private final String message;
+
+
+    public CreateNotificationRequest(UUID userId, String subject, String message) {
+        this.userId = userId;
+        this.message = message;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
