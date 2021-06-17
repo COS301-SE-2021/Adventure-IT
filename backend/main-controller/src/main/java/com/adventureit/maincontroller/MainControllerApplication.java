6c68bc2766bc2385c7644275706e902d8a381435
Alex <u19069716@tuks.co.za>
2021-06-17 19:38:17 +0200

    feat(main-controller): added main controller

---
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/MainControllerApplication.java
@@ -0,0 +1,13 @@
+package com.adventureit.maincontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainControllerApplication.class, args);
+	}
+
+}
---
+++ backend/main-controller/src/test/java/com/adventureit/maincontroller/MainControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventureit.maincontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
