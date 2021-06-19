49d38aa8be53b991165584dffeb07e0d854c5203
simrxn_r <simrathilal@gmail.com>
2021-06-19 19:20:14 +0200

    user repository for getUserByUUID

--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/Repository/UserRepository.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Repository/UserRepository.java
@@ -4,11 +4,9 @@ import com.adventureit.userservice.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    List<User> GetUserByUUID(UUID userId);
-    void RegisterUser();
+    void getUserByUserID(UUID userID);
 }
