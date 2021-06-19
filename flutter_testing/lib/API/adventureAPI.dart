8beabf0e37cb4e06cfcda8487baca4ab7772afc8
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-19 21:43:24 +0200

    Null checks in API

--- flutter_testing/lib/API/adventureAPI.dart
+++ flutter_testing/lib/API/adventureAPI.dart
@@ -5,40 +5,36 @@ import 'package:http/http.dart' as http;
 
 class AdventureApi {
 
-  static Future<List<Adventure>?> getOwnerAdventures() async {
+  static Future<List<Adventure>> getOwnerAdventures() async {
     http.Response response = await _getAdventureOwnerResponse();
-  if (response.statusCode == 200) {
-  List<Adventure>? ownerlist;
-  ownerlist = (json.decode(response.body) as List)
-      .map((i) => Adventure.fromJson(i))
-      .toList();
-  print("returned owners");
-  return ownerlist;
-  } else {
-  throw Exception('Failed to load adventures');
-  }
+    if (response!=null&&response.statusCode == 200) {
+      List<Adventure>? ownerlist;
+      ownerlist = (json.decode(response.body) as List)
+          .map((i) => Adventure.fromJson(i))
+          .toList();
+      print("returned owners");
+      return ownerlist;
+    }
   }
 
-  static Future<List<Adventure>?> getAttendeeAdventures() async {
-        http.Response response = await _getAdventureAttendeesResponse();
-    if (response.statusCode == 200) {
+  static Future<List<Adventure>> getAttendeeAdventures() async {
+    print("in here");
+    http.Response response = await _getAdventureAttendeesResponse();
+    print("out there");
+    if (response!=null&&response.statusCode == 200) {
       List<Adventure>? attendeelist;
       attendeelist = (json.decode(response.body) as List)
           .map((i) => Adventure.fromJson(i))
           .toList();
       print("returned attendees");
       return attendeelist;
-    } else {
-      throw Exception('Failed to load adventures');
     }
   }
 
   static Future<http.Response> _getAdventureAttendeesResponse() async {
-
-    return http.get(Uri.http(kApi, '/adventure/attendee/1660bd85-1c13-42c0-955c-63b1eda4e90b'));
+    return http.get(Uri.http(kApi, '/adventure/attendee/7a984756-16a5-422e-a377-89e1772dd71e'));
   }
   static Future<http.Response> _getAdventureOwnerResponse() async {
-
     return http.get(Uri.http(kApi, '/adventure/owner/1660bd85-1c13-42c0-955c-63b1eda4e90b'));
   }
-}
+}}
