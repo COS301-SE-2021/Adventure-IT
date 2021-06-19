b48333b8a507785833481f13a27d07e55aaa049f
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-19 15:20:55 +0200

    Additional API functions added

--- flutter_testing/lib/API/adventureAPI.dart
+++ flutter_testing/lib/API/adventureAPI.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
-
+import 'package:flutter_testing/constants.dart';
 import 'package:flutter_testing/API/adventures.dart';
 import 'package:http/http.dart' as http;
 
 class AdventureApi {
-  static Future<List<Adventure>> getAllAdventures() async {
-    http.Response? response = await _getAdventuresResponse();
+  static Future<List<Adventure>> getOwnerAdventures() async {
+    http.Response? response = await _getAdventuresOwnerResponse();
 
     if (response!.statusCode != 200) {
       throw Exception(
@@ -20,9 +20,29 @@ class AdventureApi {
     return adventures;
   }
 
-  static Future<http.Response?> _getAdventuresResponse() async {
+  static Future<http.Response> _getAdventuresOwnerResponse() async {
 
-    // ignore: file_names, file_names
-    return null;
+    return http.get(Uri.http(kApi, '/viewAdventures'));
+  }
+
+  static Future<List<Adventure>> getAttendeeAdventures() async {
+    http.Response? response = await _getAdventuresAttendeeResponse();
+
+    if (response!.statusCode != 200) {
+      throw Exception(
+          'Failed to load list of adventures: ${response.body}');
+    }
+    print("returned");
+    List<Adventure> adventures =
+    (jsonDecode(response.body) as List)
+        .map((fm) => Adventure.fromJson(fm))
+        .toList();
+
+    return adventures;
+  }
+
+  static Future<http.Response> _getAdventuresAttendeeResponse() async {
+
+    return http.get(Uri.http(kApi, '/viewAdventures'));
   }
 }
