42f001c715245e56d0d047c66bd68cd9a7d893fd
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-06-25 19:53:37 +0200

    Split the frontend into separate files

---
+++ alex_flutter/adventure_it/lib/frontEnd/HomepageStartup.dart
@@ -0,0 +1,48 @@
+import 'dart:html';
+
+import 'package:adventure_it/api/adventure.dart';
+import 'package:adventure_it/api/adventure_api.dart';
+import 'package:adventure_it/constants.dart';
+import 'package:adventure_it/api/budgetAPI.dart';
+
+import 'package:flutter/material.dart';
+import 'Login.dart';
+import 'Adventures.dart';
+
+import '../api/budget.dart';
+
+class HomepageStartupCaller extends StatefulWidget {
+  @override
+  HomePage createState() => HomePage();
+}
+
+class HomePage extends State<HomepageStartupCaller> {
+  Future<List<Adventure>>? ownerAdventuresFuture;
+  Future<List<Adventure>>? attendeeAdventuresFuture;
+
+  void initState() {
+    super.initState();
+    ownerAdventuresFuture = AdventureApi.getAdventuresByOwner();
+    attendeeAdventuresFuture = AdventureApi.getAdventuresByAttendee();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: Theme.of(context),
+        home: Scaffold(
+            appBar: AppBar(
+                centerTitle: true,
+                title: Text("Home Page"),
+                leading: IconButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Login()));
+                      ;
+                    },
+                    icon: const Icon(Icons.logout))),
+            body: HomePage_Pages(
+                ownerAdventuresFuture: ownerAdventuresFuture,
+                attendeeAdventuresFuture: attendeeAdventuresFuture)));
+  }
+}
---
+++ alex_flutter/adventure_it/lib/frontEnd/Login.dart
@@ -0,0 +1,52 @@
+import 'dart:html';
+
+import 'package:adventure_it/api/adventure.dart';
+import 'package:adventure_it/api/adventure_api.dart';
+import 'package:adventure_it/constants.dart';
+import 'package:adventure_it/api/budgetAPI.dart';
+
+import 'package:flutter/material.dart';
+import 'HomepageStartup.dart';
+
+import '../api/budget.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Center(child: Text("Login"))),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                child: Image.asset(
+                  "assets/adventure.PNG",
+                  scale: 1.0,
+                ),
+              ),
+              SizedBox(
+                width: 400.0,
+                child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Email')),
+              ),
+              SizedBox(
+                width: 400.0,
+                child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Password')),
+              ),
+              ElevatedButton(
+                  child: Text("Log In"),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomepageStartupCaller()),
+                    );
+                  })
+            ]));
+  }
+}
