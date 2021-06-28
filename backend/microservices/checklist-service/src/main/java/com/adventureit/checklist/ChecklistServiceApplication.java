31fa01a0a3f9fadfe83e2beb6a7e33bb4ae377fb
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-06-28 15:53:58 +0200

    feat: Added checklist service

---
+++ backend/microservices/checklist-service/src/main/java/com/adventureit/checklist/ChecklistServiceApplication.java
@@ -0,0 +1,15 @@
+package com.adventureit.checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ChecklistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistServiceApplication.class, args);
+	}
+
+}
