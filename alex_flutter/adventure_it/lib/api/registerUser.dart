a1d652942f9a51636c42e65459366a475f760379
simrxn_r <simrathilal@gmail.com>
2021-07-01 15:03:45 +0200

    created RegisterUser API class for post requests

---
+++ alex_flutter/adventure_it/lib/api/registerUser.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable(explicitToJson: true)
+class RegisterUser {
+  final String firstName;
+  final String lastName;
+  final String username;
+  final String email;
+  final String phoneNumber;
+  final String password;
+
+  RegisterUser({
+        required this.firstName,
+        required this.lastName,
+        required this.username,
+        required this.email,
+        required this.phoneNumber,
+        required this.password
+      });
+}
