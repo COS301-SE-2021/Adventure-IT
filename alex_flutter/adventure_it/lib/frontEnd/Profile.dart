a43928f13e2fd4f650d17d0b070eaf27f362b777
simrxn_r <simrathilal@gmail.com>
2021-07-05 21:14:44 +0200

    added profile picture and username template spaces for profile page

--- alex_flutter/adventure_it/lib/frontEnd/Profile.dart
+++ alex_flutter/adventure_it/lib/frontEnd/Profile.dart
@@ -23,6 +23,34 @@ class Profile extends State<ProfileCaller> {
       appBar: AppBar(title: Center(child: Text("Profile",
           style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)
       )), backgroundColor: Theme.of(context).primaryColorDark),
+      body: Row(
+        children: [ Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              width: 200,
+              height: 100,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: ExactAssetImage('assets/adventure.PNG'),
+              ),
+              decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              border: new Border.all(
+              color: Theme.of(context).accentColor,
+              width: 3.0,
+            ),),),
+          ]),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+            width: 400.0,
+            child: Text("Username",
+                style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color, fontSize: 30)),
+          ),
+        ])]
+      )
     );
   }
 }
