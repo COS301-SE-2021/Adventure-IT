2778d9eeb7327ff92432ff7f475db6bc13895232
kevichi <kevin9716cui@gmail.com>
2021-07-07 23:28:35 +0200

    feat(UserProfile.g) Implemented register Created .g file for userProfile

--- alex_flutter/adventure_it/lib/api/userProfile.g.dart
+++ alex_flutter/adventure_it/lib/api/userProfile.g.dart
@@ -9,10 +9,20 @@ part of 'userProfile.dart';
 UserProfile _$UserProfileFromJson(Map<String, dynamic> json) {
   return UserProfile(
     userID: json['userID'] as String,
+    username: json['username'] as String,
+    firstname: json['firstname'] as String,
+    lastname: json['lastname'] as String,
+    email: json['email'] as String,
+    phoneNumber: json['phoneNumber'] as String,
   );
 }
 
 Map<String, dynamic> _$UserProfileToJson(UserProfile instance) =>
     <String, dynamic>{
       'userID': instance.userID,
+      'username': instance.username,
+      'firstname': instance.firstname,
+      'lastname': instance.lastname,
+      'email': instance.email,
+      'phoneNumber': instance.phoneNumber,
     };
