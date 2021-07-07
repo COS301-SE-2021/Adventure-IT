a487b7e8bcd4c6f095e28b753f328a0a9e204290
kevichi <kevin9716cui@gmail.com>
2021-07-07 02:16:14 +0200

    feat(UserProfile.g) Implemented register Created .g file for userProfile

---
+++ alex_flutter/adventure_it/lib/api/userProfile.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'userProfile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserProfile _$UserProfileFromJson(Map<String, dynamic> json) {
+  return UserProfile(
+    userID: json['userID'] as String,
+  );
+}
+
+Map<String, dynamic> _$UserProfileToJson(UserProfile instance) =>
+    <String, dynamic>{
+      'userID': instance.userID,
+    };
