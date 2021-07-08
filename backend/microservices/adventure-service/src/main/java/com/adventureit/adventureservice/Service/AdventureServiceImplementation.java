e5577c57d52d71453074d73ae1f67a356da1cf4e
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-07-08 11:07:45 +0200

    Updated mock data to show range

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureServiceImplementation.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/Service/AdventureServiceImplementation.java
@@ -165,7 +165,7 @@ public class AdventureServiceImplementation implements AdventureService {
         final UUID mockAttendeeID = UUID.fromString("7a984756-16a5-422e-a377-89e1772dd71e");
 
         Adventure mockAdventure1 = new Adventure("Mock Adventure 1","Mock Description 1", UUID.randomUUID(), mockOwnerID, LocalDate.of(2021, 7, 5),LocalDate.of(2021, 7, 9));
-        Adventure mockAdventure2 = new Adventure("Mock Adventure 2","Mock Description 2", UUID.randomUUID(), mockOwnerID, LocalDate.of(2022, 1, 3),LocalDate.of(2022, 2, 2));
+        Adventure mockAdventure2 = new Adventure("Mock Adventure 2","Mock Description 2", UUID.randomUUID(), mockOwnerID, LocalDate.of(2021, 1, 3),LocalDate.of(2022, 2, 2));
         Adventure mockAdventure3 = new Adventure("Mock Adventure 3", "Mock Description 3",UUID.randomUUID(), mockAttendeeID, LocalDate.of(2022, 1, 4),LocalDate.of(2022, 1, 22));
 
         mockAdventure1.addContainer(UUID.fromString("d53a7090-45f1-4eb2-953a-2258841949f8"));
