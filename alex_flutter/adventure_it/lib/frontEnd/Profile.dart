6fa864dd8aafc5527187a103783d05ca7c101134
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-07-25 21:21:28 +0200

    Centered and added some MediaQuery measurements to profile page

--- alex_flutter/adventure_it/lib/frontEnd/Profile.dart
+++ alex_flutter/adventure_it/lib/frontEnd/Profile.dart
@@ -12,32 +12,35 @@ class ProfileCaller extends StatefulWidget {
 }
 
 class Profile extends State<ProfileCaller> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: AppBar(title: Center(child: Text("Profile",
-          style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)
-      )), backgroundColor: Theme.of(context).primaryColorDark),
-      body: Column(
-        children: [
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+            title: Center(
+                child: Text("Profile",
+                    style: new TextStyle(
+                        color: Theme.of(context).textTheme.bodyText1!.color))),
+            backgroundColor: Theme.of(context).primaryColorDark),
+        body: Column(children: [
           ProfileFutureBuilderCaller(),
           Container(
-            padding: const EdgeInsets.only(left: 100.0, top: 0.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              padding: const EdgeInsets.only(left: 100.0, top: 0.0),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 SizedBox(
                   width: 400.0,
                   child: Text("Friend list",
-                    style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color, fontSize:20)),
+                      style: new TextStyle(
+                          color: Theme.of(context).textTheme.bodyText1!.color,
+                          fontSize: 20)),
                 )
                 //_buildList()
-            ])),]
-        ));}
+              ])),
+        ]));
+  }
 
-  /*Widget _buildList() => ListView(
+/*Widget _buildList() => ListView(
     children: [
       _tile("Friend 1"),
       _tile("Friend 2"),
@@ -60,7 +63,7 @@ class ProfileFutureBuilderCaller extends StatefulWidget {
 }
 
 class ProfileFutureBuilder extends State<ProfileFutureBuilderCaller> {
-    Future<UserProfile>? userFuture;
+  Future<UserProfile>? userFuture;
   final UserApi api = new UserApi();
 
   @override
@@ -69,113 +72,178 @@ class ProfileFutureBuilder extends State<ProfileFutureBuilderCaller> {
     userFuture = UserApi.getUserByUUID("3f21ea6b-2288-42f3-9175-39adfafea9ab");
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return FutureBuilder(
-          future: userFuture,
-          builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator(
-              valueColor: new AlwaysStoppedAnimation<Color>(Theme
-                  .of(context)
-                  .accentColor)));
-        }
-        print(snapshot.data);
-        print(userFuture);
-        log(snapshot.data.toString());
-        if (snapshot.hasData) {
-          var user = snapshot.data as UserProfile;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: userFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).accentColor)));
+          }
+          print(snapshot.data);
+          print(userFuture);
+          log(snapshot.data.toString());
+          if (snapshot.hasData) {
+            var user = snapshot.data as UserProfile;
 
-          return Card(
-            color: Theme.of(context).primaryColorDark,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 50),
-                      Container(
-                        width: 200,
-                        height: 100,
-                        child: CircleAvatar(
-                          radius: 70,
-                          backgroundImage: ExactAssetImage('assets/adventure.PNG'),
-                        ),
-                        decoration: new BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: new Border.all(
-                            color: Theme.of(context).accentColor,
-                            width: 3.0,
-                          ),),),
-                      Container(
-                        padding: const EdgeInsets.only(left: 100.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(height: 50),
-                            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            SizedBox(
-                              width: 400.0,
-                              child: Text(user.firstname + " " + user.lastname,
-                                  textAlign: TextAlign.center,
-                                  style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color, fontSize: 30)),
+            return Container(
+                margin: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.55,
+                child: Card(
+                    color: Theme.of(context).primaryColorDark,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Center(
+                              child: Column(children: <Widget>[
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.2,
+                              height: MediaQuery.of(context).size.height * 0.2,
+                              child: CircleAvatar(
+                                radius: 70,
+                                backgroundImage:
+                                    ExactAssetImage('assets/adventure.PNG'),
+                              ),
+                              decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: new Border.all(
+                                  color: Theme.of(context).accentColor,
+                                  width: 3.0,
+                                ),
+                              ),
                             ),
-                            SizedBox(
-                              width: 400.0,
-                              child: Text(user.username,
-                                  textAlign: TextAlign.center,
-                                  style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color, fontSize: 20)),
-                            ),
-                            SizedBox(height: 25),
-                            SizedBox(
-                              width: 600.0,
-                              child: Text(user.email + "      " + user.phoneNumber,
-                                  textAlign: TextAlign.center,
-                                  style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color, fontSize:20)),
-                            ),
-                            SizedBox(height: 50),
-                          ],)),
-                      MaterialButton(
-                          child: Text("My Documents",
-                              style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)),
-                          color: Theme.of(context).accentColor,
-                          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                            Container(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.01),
+                                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomepageStartupCaller()),
-                            );
-                          }),
-                      SizedBox(height: 10),
-                      MaterialButton(
-                          onPressed: () => {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomepageStartupCaller()),
-                            )},
-                          color: Theme.of(context).accentColor,
-                          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(Icons.settings, color: Theme.of(context).textTheme.bodyText1!.color),
-                              Text("Edit Profile",
-                                  style: new TextStyle(color: Theme.of(context).textTheme.bodyText1!.color)) ],
-                          )),
-                      SizedBox(height: 10)
-                      ]))
-                  ])
-          );
-        }
+                                  Text(user.firstname + " " + user.lastname,
+                                      textAlign: TextAlign.center,
+                                      style: new TextStyle(
+                                          color: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1!
+                                              .color,
+                                          fontSize: 30)),
+                                  Text(user.username,
+                                      textAlign: TextAlign.center,
+                                      style: new TextStyle(
+                                          color: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1!
+                                              .color,
+                                          fontSize: 20)),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.02),
+                                  Row(children: [
+                                    Expanded(
+                                        child: Text(user.email,
+                                            textAlign: TextAlign.center,
+                                            style: new TextStyle(
+                                                color: Theme.of(context)
+                                                    .textTheme
+                                                    .bodyText1!
+                                                    .color,
+                                                fontSize: 20))),
+                                    Expanded(
+                                        child: Text(user.phoneNumber,
+                                            textAlign: TextAlign.center,
+                                            style: new TextStyle(
+                                                color: Theme.of(context)
+                                                    .textTheme
+                                                    .bodyText1!
+                                                    .color,
+                                                fontSize: 20))),
+                                  ]),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.03),
+                                  Row(children: [
+                                    Expanded(
+                                        child: Container(
+                                            margin: EdgeInsets.symmetric(
+                                                horizontal:
+                                                    MediaQuery.of(context)
+                                                            .size
+                                                            .width *
+                                                        0.1),
+                                            child: MaterialButton(
+                                                child: Text("My Documents",
+                                                    style: new TextStyle(
+                                                        color: Theme.of(context)
+                                                            .textTheme
+                                                            .bodyText1!
+                                                            .color)),
+                                                color: Theme.of(context)
+                                                    .accentColor,
+                                                padding: EdgeInsets.symmetric(
+                                                    horizontal: 50,
+                                                    vertical: 20),
+                                                onPressed: () {
+                                                  Navigator.pushReplacement(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            HomepageStartupCaller()),
+                                                  );
+                                                }))),
+                                    Expanded(
+                                      child: Container(
+                                          margin: EdgeInsets.symmetric(
+                                              horizontal: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.1),
+                                          child: MaterialButton(
+                                              onPressed: () => {
+                                                    Navigator.pushReplacement(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              HomepageStartupCaller()),
+                                                    )
+                                                  },
+                                              color:
+                                                  Theme.of(context).accentColor,
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 50,
+                                                  vertical: 20),
+                                              child:
+                                                  Text("Edit Profile",
+                                                      textAlign: TextAlign.center,
+                                                      style: new TextStyle(
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .textTheme
+                                                                  .bodyText1!
+                                                                  .color))
 
-        else
-        {
-          return Center(child: Text("Profile has not been created", style: TextStyle(fontSize: 30, color:Theme.of(context).textTheme.bodyText1!.color)));
-        }
-      });
-    }
+                                              ))),
+                                    ])
+                                ]))
+                          ]))
+                        ])));
+          } else {
+            return Center(
+                child: Text("Profile has not been created",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Theme.of(context).textTheme.bodyText1!.color)));
+          }
+        });
+  }
 }
