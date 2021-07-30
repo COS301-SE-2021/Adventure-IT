c8534c8385ace3399b68325df70b0f5f3dab8476
kevichi <kevin9716cui@gmail.com>
2021-07-30 21:29:24 +0200

    fix(BudgetService): Debugging budget microservice

--- backend/microservices/itinerary-service/src/main/java/com/adventureit/itinerary/Entity/ItineraryEntry.java
+++ backend/microservices/itinerary-service/src/main/java/com/adventureit/itinerary/Entity/ItineraryEntry.java
@@ -2,10 +2,7 @@ package com.adventureit.itinerary.Entity;
 
 import com.adventureit.adventureservice.Entity.Entry;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 /**
  *
