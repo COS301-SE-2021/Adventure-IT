a517ce38e859518212cb0b6629e310308ddb7209
kevichi <kevin9716cui@gmail.com>
2021-08-01 16:19:37 +0200

    feat(TimelineService): Creted new microservice and entity

--- backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Entity/Timeline.java
+++ backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Entity/Timeline.java
@@ -1,4 +1,69 @@
 package com.adventureit.timelineservice.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
 public class Timeline {
+    @Id
+    private UUID timelineID;
+    private UUID adventureID;
+    private UUID userID;
+    private String descrpition;
+    private LocalDateTime timestamp;
+
+
+    public Timeline(UUID timelineID, UUID adventureID, UUID userID, String descrpition, LocalDateTime timestamp) {
+        this.timelineID = timelineID;
+        this.adventureID = adventureID;
+        this.userID = userID;
+        this.descrpition = descrpition;
+        this.timestamp = timestamp;
+    }
+
+    public Timeline(){
+
+    }
+
+    public UUID getTimelineID() {
+        return timelineID;
+    }
+
+    public void setTimelineID(UUID timelineID) {
+        this.timelineID = timelineID;
+    }
+
+    public UUID getAdventureID() {
+        return adventureID;
+    }
+
+    public void setAdventureID(UUID adventureID) {
+        this.adventureID = adventureID;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getDescrpition() {
+        return descrpition;
+    }
+
+    public void setDescrpition(String descrpition) {
+        this.descrpition = descrpition;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
