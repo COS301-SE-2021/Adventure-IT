1c1f1b5049255210b8cfa4d82ed46a81feb386b7
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-08-02 00:45:59 +0200

    Checklist and Itinerary entry entities made

--- alex_flutter/adventure_it/lib/api/itineraryEntry.dart
+++ alex_flutter/adventure_it/lib/api/itineraryEntry.dart
@@ -1,28 +1,32 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:time_machine/time_machine.dart';
 import '/api/adventure.dart';
 import '/api/transactions.dart';
 
-// part 'itineraryEntry.g.dart';
+part 'itineraryEntry.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class itineraryEntry {
-  // final String title;
-  // final String description;
-  // final String id;
-  // final String creatorID;
-  // final String adventureID;
-  // final bool deleted;
-  //
-  // itineraryEntry({
-  //   required this.title,
-  //   required this.description,
-  //   required this.id,
-  //   required this.creatorID,
-  //   required this.adventureID,
-  //   required this.deleted
-  // });
-  //
-  // factory ItineraryEntry.fromJson(Map<String, dynamic> json) => _$ItineraryEntryFromJson(json);
-  //
-  // Map<String, dynamic> toJson() => _$ItineraryEntryToJson(this);
+
+  final String id;
+  final String entryContainerID;
+  final String title;
+  final String description;
+  final bool completed;
+  final String location;
+  final LocalDateTime timestamp;
+
+  itineraryEntry({
+    required this.title,
+    required this.description,
+    required this.id,
+    required this.entryContainerID,
+    required this.completed,
+    required this.location,
+  required this.timestamp
+  });
+
+  factory ItineraryEntry.fromJson(Map<String, dynamic> json) => _$ItineraryEntryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ItineraryEntryToJson(this);
 }
