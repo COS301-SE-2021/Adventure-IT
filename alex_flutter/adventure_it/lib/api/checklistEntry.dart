98322b7c004e5e38833b6c16ac3e7dc9feec7199
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-08-01 20:35:34 +0200

    Entities made for entries of checklist and itinerary

---
+++ alex_flutter/adventure_it/lib/api/checklistEntry.dart
@@ -0,0 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+import '/api/adventure.dart';
+import '/api/transactions.dart';
+
+// part 'checklistEntry.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class ChecklistEntry {
+  // final String title;
+  // final String description;
+  // final String id;
+  // final String creatorID;
+  // final String adventureID;
+  // final bool deleted;
+  //
+  // ChecklistEntry({
+  //   required this.title,
+  //   required this.description,
+  //   required this.id,
+  //   required this.creatorID,
+  //   required this.adventureID,
+  //   required this.deleted
+  // });
+  //
+  // factory ChecklistEntry.fromJson(Map<String, dynamic> json) => _$ChecklistEntryFromJson(json);
+  //
+  // Map<String, dynamic> toJson() => _$ChecklistEntryToJson(this);
+}
---
+++ alex_flutter/adventure_it/lib/api/itineraryEntry.dart
@@ -0,0 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+import '/api/adventure.dart';
+import '/api/transactions.dart';
+
+// part 'itineraryEntry.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class itineraryEntry {
+  // final String title;
+  // final String description;
+  // final String id;
+  // final String creatorID;
+  // final String adventureID;
+  // final bool deleted;
+  //
+  // itineraryEntry({
+  //   required this.title,
+  //   required this.description,
+  //   required this.id,
+  //   required this.creatorID,
+  //   required this.adventureID,
+  //   required this.deleted
+  // });
+  //
+  // factory ItineraryEntry.fromJson(Map<String, dynamic> json) => _$ItineraryEntryFromJson(json);
+  //
+  // Map<String, dynamic> toJson() => _$ItineraryEntryToJson(this);
+}
