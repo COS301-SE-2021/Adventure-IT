2aba9c7a8e40e862674f4098b924e93cc7adf808
Alex <u19069716@tuks.co.za>
2021-08-02 01:15:55 +0200

    feat(front-end): implemented Firebase Messaging

--- alex_flutter/adventure_it/lib/main.dart
+++ alex_flutter/adventure_it/lib/main.dart
@@ -6,11 +6,11 @@ import 'package:adventure_it/api/budgetAPI.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
+import 'Firebase/InitializeFireFlutter.dart';
 import 'Providers/adventure_model.dart';
 import 'api/budget.dart';
 import 'frontEnd/Login.dart';
 import 'frontEnd/HomepageStartup.dart';
-
 
 void main() {
   runApp(
@@ -20,34 +20,26 @@ void main() {
           create: (context) => AdventuresModel(),
         ),
       ],
-      child: MyApp(),
+      child: InitializeFireFlutter(MyApp()),
     ),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme:  ThemeData(
-          primaryColorLight: Color(0xff484D64),
-          primaryColorDark: Color(0xff323647),
-          scaffoldBackgroundColor: Color(0xff20222D),
-          accentColor: Color(0xff6A7AC7),
-          primaryColor: Color(0xff808080) ,
-          textSelectionTheme: TextSelectionThemeData(selectionColor: Color(0xffA7AAB9)),
-          textTheme: TextTheme(
-            bodyText1: TextStyle(color: Color(0xffA7AAB9)),
-            bodyText2: TextStyle(color:Color(0xff20222D))
-          )),
+        theme: ThemeData(
+            primaryColorLight: Color(0xff484D64),
+            primaryColorDark: Color(0xff323647),
+            scaffoldBackgroundColor: Color(0xff20222D),
+            accentColor: Color(0xff6A7AC7),
+            primaryColor: Color(0xff808080),
+            textSelectionTheme:
+                TextSelectionThemeData(selectionColor: Color(0xffA7AAB9)),
+            textTheme: TextTheme(
+                bodyText1: TextStyle(color: Color(0xffA7AAB9)),
+                bodyText2: TextStyle(color: Color(0xff20222D)))),
         home: LoginCaller());
   }
 }
-
-
-
-
-
-
-
