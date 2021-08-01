81eef6cc5b662b04a4127829a86b8de2bf52ed75
kevichi <kevin9716cui@gmail.com>
2021-08-01 17:35:53 +0200

    feat(TimelineService): Created TimelineDTO as well as updatee the repository

--- backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Repository/TimelineRepository.java
+++ backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Repository/TimelineRepository.java
@@ -3,8 +3,11 @@ package com.adventureit.timelineservice.Repository;
 import com.adventureit.timelineservice.Entity.Timeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TimelineRepository extends JpaRepository<Timeline, UUID> {
-
+    List<Timeline> findAllByAdventureID(UUID adventureID);
+    Timeline findByTimelineID(UUID timelineID);
+    void removeAllByAdventureID(UUID adventureID);
 }
--- backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Service/TimelineServiceImplementation.java
+++ backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Service/TimelineServiceImplementation.java
@@ -1,5 +1,57 @@
 package com.adventureit.timelineservice.Service;
 
+import com.adventureit.timelineservice.Entity.Timeline;
+import com.adventureit.timelineservice.Entity.TimelineType;
+import com.adventureit.timelineservice.Repository.TimelineRepository;
+import com.adventureit.timelineservice.Responses.TimelineDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class TimelineServiceImplementation {
+
+
+    private TimelineRepository repo;
+
+    @Autowired
+    public TimelineServiceImplementation(TimelineRepository repo) {
+        this.repo = repo;
+    }
+
+    public String createTimelineEntry(UUID adventureID, UUID userID, String description, TimelineType type){
+        UUID timelineID = UUID.randomUUID();
+        LocalDateTime time= LocalDateTime.now();
+        repo.save(new Timeline(timelineID,adventureID,userID,description,time,type));
+        return "New timeline entry created for adventure "+adventureID+" by user "+userID;
+    }
+
+    public List<TimelineDTO> GetTimelineByAdventureID(UUID adventureID){
+        List<Timeline> list =repo.findAllByAdventureID(adventureID);
+
+        if(list == null){
+            /* throw timeline doesnot exist exception*/
+        }
+        List<TimelineDTO> returnList = new ArrayList<>();
+        for(Timeline entry: list){
+            returnList.add(new TimelineDTO(entry.getTimelineID(),entry.getAdventureID(),entry.getUserID(),entry.getDescrpition(),entry.getTimestamp(),entry.getType()));
+        }
+        return returnList;
+    }
+
+    public String deleteTimelineByAdventureID(UUID adventureID){
+        List<Timeline> list = repo. findAllByAdventureID(adventureID);
+        if(list == null){
+            /*throw Timeline doesnot exist for adventure*/
+        }
+
+        repo.removeAllByAdventureID(adventureID);
+        return "Timeline for adventure: "+adventureID+" has been deleted";
+    }
+
 
 }
