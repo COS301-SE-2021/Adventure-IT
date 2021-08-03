7a720435216c111159c4ccb39f62b12f9a93b029
Alex <u19069716@tuks.co.za>
2021-08-03 12:13:14 +0200

    feat(main-controller): enabled keycloak authentication on all incoming requests

--- backend/main-controller/src/main/java/com/adventureit/maincontroller/Security/SecurityConfig.java
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,8 +12,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(auth -> auth
-                .antMatchers(HttpMethod.GET, "/**").permitAll()
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+                .antMatchers(HttpMethod.GET, "/**").authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
