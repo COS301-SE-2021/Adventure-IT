40468214457abf1dfe7006f1d38a1b4663d11588
Alex <u19069716@tuks.co.za>
2021-08-03 23:46:53 +0200

    feat(front-end): Added keycloak constants & methods

--- alex_flutter/adventure_it/lib/api/user_api.dart
+++ alex_flutter/adventure_it/lib/api/user_api.dart
@@ -10,7 +10,13 @@ import 'loginUser.dart';
 import 'userProfile.dart';
 
 class UserApi {
-  Future<RegisterUser> createUser(String firstName,String lastName,String username,String email,String phoneNumber,String password) async {
+  Future<RegisterUser> createUser(
+      String firstName,
+      String lastName,
+      String username,
+      String email,
+      String phoneNumber,
+      String password) async {
     final response = await http.post(
       Uri.parse('http://localhost:9002/api/RegisterUser'), //get uri
       headers: <String, String>{
@@ -24,8 +30,6 @@ class UserApi {
         'phoneNumber': phoneNumber,
         'password': password,
       }),
-
-
     );
 
     if (response.statusCode == 201) {
@@ -33,7 +37,13 @@ class UserApi {
       // then parse the JSON.
       print('Status code: ${response.statusCode}');
       print('Body: ${response.body}');
-      return RegisterUser(firstName: firstName, lastName: lastName, username: username, email: email, phoneNumber: phoneNumber, password: password);
+      return RegisterUser(
+          firstName: firstName,
+          lastName: lastName,
+          username: username,
+          email: email,
+          phoneNumber: phoneNumber,
+          password: password);
     } else {
       // If the server did not return a 201 CREATED response,
       // then throw an exception.
@@ -66,15 +76,33 @@ class UserApi {
 
   static Future<UserProfile> getUserByUUID(String userID) async {
     print("in function");
-    http.Response response = await http.get(Uri.parse('http://localhost:9002/api/GetUser/' + userID));
+    http.Response response = await http
+        .get(Uri.parse('http://localhost:9002/api/GetUser/' + userID));
 
     if (response.statusCode != 200) {
       throw Exception('Failed to load user information: ${response.body}');
     }
     print(response.body);
 
-    UserProfile users = (UserProfile.fromJson(jsonDecode(response.body)) );
+    UserProfile users = (UserProfile.fromJson(jsonDecode(response.body)));
     print(users.username);
     return users;
   }
+
+  Future<String?> attemptLogIn(String username, String password) async {
+    var res = await http.post(Uri.parse(authApiGetToken), body: {
+      "client_id": "adventure-it-maincontroller",
+      "username": username,
+      "password": password,
+      "grant_type": "password"
+    });
+    if (res.statusCode == 200) return res.body;
+    return null;
+  }
+
+  // Future<int> attemptSignUp(String username, String password) async {
+  //   var res = await http.post(Uri.parse("$SERVER_IP/signup"),
+  //       body: {"username": username, "password": password});
+  //   return res.statusCode;
+  // }
 }
--- alex_flutter/adventure_it/lib/constants.dart
+++ alex_flutter/adventure_it/lib/constants.dart
@@ -4,3 +4,6 @@ final adventureApi = kIsWeb ? "localhost:9001" : "10.0.2.2:9001";
 final budgetApi = kIsWeb ? "localhost:9007" : "10.0.2.2:9007";
 final itineraryApi = kIsWeb ? "localhost:9009" : "10.0.2.2:9009";
 final checklistApi = kIsWeb ? "localhost:9008" : "10.0.2.2:9008";
+final authApiGetToken = kIsWeb
+    ? "localhost:8080/auth/realms/adventure-it/protocol/openid-connect/token"
+    : "10.0.2.2:8080/auth/realms/adventure-it/protocol/openid-connect/token";
