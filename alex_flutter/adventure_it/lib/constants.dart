5578d4469bc07738af67366d842348336be147e0
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-08-03 19:55:07 +0200

    LocationApi created

--- alex_flutter/adventure_it/lib/constants.dart
+++ alex_flutter/adventure_it/lib/constants.dart
@@ -4,3 +4,5 @@ final adventureApi = kIsWeb ? "localhost:9001" : "10.0.2.2:9001";
 final budgetApi = kIsWeb ? "localhost:9007" : "10.0.2.2:9007";
 final itineraryApi = kIsWeb ? "localhost:9009" : "10.0.2.2:9009";
 final checklistApi = kIsWeb ? "localhost:9008" : "10.0.2.2:9008";
+
+final googleMapsKey="Aap_uEAZc3J88tXDBSsnkxZm-CIiWLywW-UYCC9bwuwAo4McCI_a-i1j_mUCp3hZ6gU8zmKeMwYFS94CVgBdOjv0dtPG50vX1tanap-4XFThrJR_YgkgIhczSCuCjSNs6W6o20-4kWYMYEFpF6nHknoeuXU6KgzIW1I2O-m6vzYg9E-UtcJH";
