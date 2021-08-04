44e82b25c5fe4b0517386d7e469797757ff2ccd1
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-08-04 21:26:09 +0200

    feat(MainController): get Attendees of Adventure implemented

--- backend/main-controller/src/main/java/com/adventureit/maincontroller/Controller/MainController.java
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/Controller/MainController.java
@@ -1,22 +1,25 @@
 package com.adventureit.maincontroller.Controller;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/main")
 public class MainController {
 
 //    @Autowired
 //    private EurekaClient eurekaClient;
 
     private RestTemplate restTemplate = new RestTemplate();
+    private final String IP = "localhost";
+    private final String adventurePort = "9001";
+    private final String locationPort = "9006";
 
 //    @RequestMapping("/adventure/test")
 //    public String adventureTest(){
@@ -26,10 +29,11 @@ public class MainController {
 //        return restTemplate.getForObject("http://"+ adventureIP + ":" + adventurePort + "/adventure/test", String.class);
 //    }
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String mainControllerTest(){
         return "Main controller is working";
     }
+
 /*
     @RequestMapping("/api/GetUser/{id}")
     public GetUserByUUIDResponse getUserByUUID(@PathVariable UUID id){
@@ -38,6 +42,19 @@ public class MainController {
         int adventurePort = adventureInstance.getPort();
         return restTemplate.getForObject("http://"+ adventureIP + ":" + adventurePort + "/adventure/test", String.class);
     }*/
+
+    @GetMapping("/adventure/test")
+    public String adventureTest(){
+        return restTemplate.getForObject("http://"+ IP + ":" + adventurePort + "/adventure/test", String.class);
+    }
+
+    @GetMapping("adventure/getAttendees/{id}")
+    public List getAttendees(@PathVariable UUID id) throws Exception {
+        return restTemplate.getForObject("http://"+ IP + ":" + adventurePort + "/adventure/getAttendees/" + id, List.class);
+    }
+
+    
+
 }
 
 
