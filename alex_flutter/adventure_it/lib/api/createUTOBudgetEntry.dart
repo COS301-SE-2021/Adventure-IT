613062c284032e3171cf2df031d861c73b4e04f7
Simran <simrathilal@gmail.com>
2021-08-07 21:19:05 +0200

    created UTO budget entry API class

---
+++ alex_flutter/adventure_it/lib/api/createUTOBudgetEntry.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+import '/api/adventure.dart';
+import '/api/transactions.dart';
+
+part 'createUTOBudgetEntry.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class CreateUTOBudgetEntry {
+  final String entryContainerID;
+  final String payer;
+  final String amount;
+  final String title;
+  final String description;
+  final String category;
+  final String payee;
+
+  CreateUTOBudgetEntry({
+    required this.entryContainerID,
+    required this.payer,
+    required this.amount,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.payee
+  });
+
+  factory CreateUTOBudgetEntry.fromJson(Map<String, dynamic> json) => _$CreateUTOBudgetEntryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CreateUTOBudgetEntryToJson(this);
+}
