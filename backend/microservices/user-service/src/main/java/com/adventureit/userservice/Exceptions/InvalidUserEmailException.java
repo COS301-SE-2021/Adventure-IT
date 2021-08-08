ff762aebc8df40add0f95358d63e7a5dbebd5a15
kevichi <kevin9716cui@gmail.com>
2021-08-08 15:57:01 +0200

    feat(MainController):added reason message for user exceptions

--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/Exceptions/InvalidUserEmailException.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/Exceptions/InvalidUserEmailException.java
@@ -1,5 +1,9 @@
 package com.adventureit.userservice.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid User email")
 public class InvalidUserEmailException extends RuntimeException {
 
     public InvalidUserEmailException(String message){
