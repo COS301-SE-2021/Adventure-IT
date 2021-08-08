3dd128bb043d74e9e151f1b869ae31f4c1603b14
Simran <simrathilal@gmail.com>
2021-08-09 01:21:06 +0200

    initial posting for createItineraryEntry

--- alex_flutter/adventure_it/lib/api/itineraryAPI.dart
+++ alex_flutter/adventure_it/lib/api/itineraryAPI.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:adventure_it/api/createItineraryEntry.dart';
 import 'package:adventure_it/constants.dart';
 import '/api/itinerary.dart';
 import '/api/adventure.dart';
@@ -166,6 +167,38 @@ class ItineraryApi {
       print('Status code: ${response.statusCode}');
       print('Body: ${response.body}');
       throw Exception('Failed to create an itinerary.');
+    }
+  }
+
+  static Future<CreateItineraryEntry> createItineraryEntry(String entryContainerID, String title, String description, String location, String timestamp) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:9999/itinerary/addEntry'), //get uri
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'entryContainerID': entryContainerID,
+        'title': title,
+        'description': description,
+        'location': location,
+        'timestamp': timestamp
+      }),
+
+
+    );
+
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      print('Status code: ${response.statusCode}');
+      print('Body: ${response.body}');
+      return CreateItineraryEntry(entryContainerID: entryContainerID, title: title, description: description, location: location, timestamp: timestamp);
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      print('Status code: ${response.statusCode}');
+      print('Body: ${response.body}');
+      throw Exception('Failed to create an itinerary entry.');
     }
   }
 }
