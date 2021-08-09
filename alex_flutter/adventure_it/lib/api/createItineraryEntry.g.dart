d3735abd3ab57397ccb075c0f24594d6104991bd
kevichi <kevin9716cui@gmail.com>
2021-08-09 22:23:46 +0200

    feat(MainController):debugging main controller adventure and itinerary reroutes

---
+++ alex_flutter/adventure_it/lib/api/createItineraryEntry.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'createItineraryEntry.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreateItineraryEntry _$CreateItineraryEntryFromJson(Map<String, dynamic> json) {
+  return CreateItineraryEntry(
+    entryContainerID: json['entryContainerID'] as String,
+    title: json['title'] as String,
+    description: json['description'] as String,
+    location: json['location'] as String,
+    timestamp: json['timestamp'] as String,
+  );
+}
+
+Map<String, dynamic> _$CreateItineraryEntryToJson(
+        CreateItineraryEntry instance) =>
+    <String, dynamic>{
+      'entryContainerID': instance.entryContainerID,
+      'title': instance.title,
+      'description': instance.description,
+      'location': instance.location,
+      'timestamp': instance.timestamp,
+    };
---
+++ alex_flutter/adventure_it/lib/api/friendRequest.dart
@@ -0,0 +1,30 @@
+import 'package:adventure_it/api/userProfile.dart';
+import 'package:json_annotation/json_annotation.dart';
+import '/api/adventure.dart';
+import '/api/transactions.dart';
+
+part 'friendRequest.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class FriendRequest {
+
+  final String id;
+  final String firstUser;
+  final String secondUser;
+  final String createdDate;
+  final bool accepted;
+  final String requester;
+
+  FriendRequest({
+    required this.id,
+    required this.firstUser,
+    required this.secondUser,
+    required this.createdDate,
+    required this.accepted,
+    required this.requester,
+  });
+
+  factory FriendRequest.fromJson(Map<String, dynamic> json) => _$FriendRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FriendRequestToJson(this);
+}
