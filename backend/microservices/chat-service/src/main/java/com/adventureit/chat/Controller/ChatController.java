1406dbeb4ceef761500a4be10700f0a266e51f69
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-08-10 21:53:33 +0200

    feat: getGroupChat function implemented and returns MessageDTO and UserDTO

--- backend/microservices/chat-service/src/main/java/com/adventureit/chat/Controller/ChatController.java
+++ backend/microservices/chat-service/src/main/java/com/adventureit/chat/Controller/ChatController.java
@@ -1,10 +1,14 @@
 package com.adventureit.chat.Controller;
 
+import com.adventureit.chat.Entity.Message;
 import com.adventureit.chat.Requests.CreateDirectChatRequest;
 import com.adventureit.chat.Requests.CreateGroupChatRequest;
+import com.adventureit.chat.Responses.GroupChatResponseDTO;
 import com.adventureit.chat.Service.ChatServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/chat")
@@ -14,22 +18,30 @@ public class ChatController {
     private ChatServiceImplementation service;
 
     @GetMapping("/test")
-    String test(){
-        return "Budget Controller is functioning";
+    public String test(){
+        return "Chat Controller is functioning";
     }
 
     @PostMapping("/createDirectChat")
-    String createDirectChat(@RequestBody CreateDirectChatRequest req){
+    public String createDirectChat(@RequestBody CreateDirectChatRequest req){
         service.createDirectChat(req.getAdventureId(),req.getUser1Id(),req.getUser2Id());
         return "Chat created";
     }
 
     @PostMapping("/createGroupChat")
-    String createDirectChat(@RequestBody CreateGroupChatRequest req){
+    public String createDirectChat(@RequestBody CreateGroupChatRequest req){
         service.createGroupChat(req.getAdventureId(),req.getParticipants(),req.getName());
         return "Group Chat created";
     }
 
+    @GetMapping("/getGroupChat/{id}")
+    public GroupChatResponseDTO getGroupChat(@PathVariable UUID id) throws Exception {
+        return service.getGroupChat(id);
+    }
 
+    @GetMapping("/getMessageByID/{id}")
+    public Message getMessage(@PathVariable UUID id){
+        return service.getMessage(id);
+    }
 
 }
