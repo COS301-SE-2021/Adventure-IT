e9015fdef8d4fd16ac22edff049356c2500e1d77
kevichi <kevin9716cui@gmail.com>
2021-08-10 22:00:01 +0200

    feat(MainController):updated budget maincontroller

--- backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Repository/TimelineRepository.java
+++ backend/microservices/timeline-service/src/main/java/com/adventureit/timelineservice/Repository/TimelineRepository.java
@@ -2,10 +2,12 @@ package com.adventureit.timelineservice.Repository;
 
 import com.adventureit.timelineservice.Entity.Timeline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TimelineRepository extends JpaRepository<Timeline, UUID> {
     List<Timeline> findAllByAdventureID(UUID adventureID);
     Timeline findByTimelineID(UUID timelineID);
