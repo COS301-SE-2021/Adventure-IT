3ead711504f95de8ad69294ab7f4a76af6030df3
Alex <u19069716@tuks.co.za>
2021-08-10 12:46:50 +0200

    feat(media service): test implementation of uploading & downloading

--- backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
+++ backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
@@ -1,14 +1,51 @@
 package com.adventureit.mediaservice.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.adventureit.mediaservice.Entity.Media;
+import com.adventureit.mediaservice.Repository.MediaRepository;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/media")
 public class MediaController {
+    @Autowired
+    private MediaRepository mediaRepository;
+
     @GetMapping("/test")
-    String test(){
+    public String test(){
         return "Media Controller is functional";
+    }
+
+    @GetMapping("/test/uploaded")
+    public ResponseEntity<byte[]> testUploaded(){
+        HttpHeaders headers = new HttpHeaders();
+        // Fetch byte array from backend & populate media variable here
+        Media storedMedia = mediaRepository.findMediaById(UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"));
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue()); // disabling caching for client who requests the resource
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(storedMedia.getData(), headers, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @PostMapping("/test/upload")
+    public HttpStatus testUploaded(@RequestParam("file") MultipartFile file){
+        try {
+            final byte[] content = file.getBytes();
+            Media uploadedMedia = new Media(UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"), "TEST TYPE", "TEST NAME", "TEST DESCRIPTION", UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"), UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"));
+            mediaRepository.save(uploadedMedia);
+            return HttpStatus.OK;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return HttpStatus.BAD_REQUEST;
+        }
+
     }
 }
