560c8eda389fa55e113b70998f8bd8c684bc01dd
Alex <u19069716@tuks.co.za>
2021-08-11 18:43:53 +0200

    feat(media service): updated MediaController to utilize MediaInfo class

--- backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
+++ backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
@@ -1,16 +1,16 @@
 package com.adventureit.mediaservice.Controller;
 
 import com.adventureit.mediaservice.Entity.Media;
+import com.adventureit.mediaservice.Entity.MediaInfo;
+import com.adventureit.mediaservice.Repository.MediaInfoRepository;
 import com.adventureit.mediaservice.Repository.MediaRepository;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,29 +19,43 @@ public class MediaController {
     @Autowired
     private MediaRepository mediaRepository;
 
+    @Autowired
+    private MediaInfoRepository mediaInfoRepository;
+
     @GetMapping("/test")
     public String test(){
         return "Media Controller is functional";
     }
 
-    @GetMapping(value = "/test/uploaded/{file}",
-            produces = { "application/pdf" })
-    public ResponseEntity<byte[]> testUploaded(@PathVariable String file){
+    @GetMapping(value = "/uploaded/{file}")
+    public ResponseEntity<byte[]> testUploaded(@PathVariable UUID file){
         HttpHeaders headers = new HttpHeaders();
-        System.out.println("Fetching file " + file); // debug
-        Media storedMedia = mediaRepository.findMediaById(UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"));
+        Media storedMedia = mediaRepository.findMediaById(file);
         headers.setCacheControl(CacheControl.noCache().getHeaderValue()); // disabling caching for client who requests the resource
+        headers.setContentType(MediaType.parseMediaType(storedMedia.getType()));
         ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(storedMedia.getData(), headers, HttpStatus.OK);
         return responseEntity;
     }
 
-    @PostMapping(value = "/test/upload")
-    public HttpStatus testUploaded(@RequestParam("file") MultipartFile file){
+    @GetMapping(value = "/getUserMediaList/{id}")
+    public List<MediaInfo> getUserMediaList(@PathVariable UUID id){
+        return mediaInfoRepository.findAllByOwner(id);
+    }
+
+    @GetMapping(value = "/getAdventureFileList/{id}")
+    public List<MediaInfo> getAdventureFileList(@PathVariable UUID id){
+        return mediaInfoRepository.findAllByAdventureID(id);
+    }
+
+    @PostMapping("/upload")
+    public HttpStatus uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("userid") UUID userId, @RequestParam("adventureid") UUID adventureId){
         try {
             final byte[] content = file.getBytes();
-            Media uploadedMedia = new Media(UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"), "TEST TYPE", "TEST NAME", "TEST DESCRIPTION", UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"), UUID.fromString("a8d9ce1d-65ee-4eb2-8003-4ce996b89c8d"));
+            Media uploadedMedia = new Media(UUID.randomUUID(), file.getContentType(), file.getName(), "DESCRIPTION", adventureId , userId);
+            MediaInfo uploadedMediaInfo = new MediaInfo(uploadedMedia.getId(), uploadedMedia.getType(), uploadedMedia.getName(), uploadedMedia.getDescription(), uploadedMedia.getAdventureID(), uploadedMedia.getOwner());
             uploadedMedia.setData(content);
             mediaRepository.save(uploadedMedia);
+            mediaInfoRepository.save(uploadedMediaInfo);
             return HttpStatus.OK;
         }
         catch(Exception e){
