23b82f8477bac960f902b975506684d52f49b472
Simran <simrathilal@gmail.com>
2021-08-15 20:13:23 +0200

    checklist creates and edits works with real userID and .g files generated

---
+++ alex_flutter/adventure_it/lib/api/report.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'report.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Report _$ReportFromJson(Map<String, dynamic> json) {
+  return Report(
+    amount: (json['amount'] as num).toDouble(),
+    payeeName: json['payeeName'] as String,
+  );
+}
+
+Map<String, dynamic> _$ReportToJson(Report instance) => <String, dynamic>{
+      'payeeName': instance.payeeName,
+      'amount': instance.amount,
+    };
