450c6ef078b777f47e14666ad2a377ec9aec5d74
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-08-16 15:30:26 +0200

    feat: MediaService and MediaServiceImplementation added

--- backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
+++ backend/microservices/media-service/src/main/java/com/adventureit/mediaservice/Controller/MediaController.java
@@ -1,13 +1,12 @@
 package com.adventureit.mediaservice.Controller;
 
-import com.adventureit.mediaservice.Entity.File;
 import com.adventureit.mediaservice.Entity.FileInfo;
-import com.adventureit.mediaservice.Entity.Media;
 import com.adventureit.mediaservice.Entity.MediaInfo;
 import com.adventureit.mediaservice.Repository.FileInfoRepository;
 import com.adventureit.mediaservice.Repository.FileRepository;
 import com.adventureit.mediaservice.Repository.MediaInfoRepository;
 import com.adventureit.mediaservice.Repository.MediaRepository;
+import com.adventureit.mediaservice.Service.MediaServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -20,16 +19,19 @@ import java.util.UUID;
 @RequestMapping("/media")
 public class MediaController {
     @Autowired
-    private MediaRepository mediaRepository;
+    private MediaServiceImplementation mediaServiceImplementation;
 
     @Autowired
-    private MediaInfoRepository mediaInfoRepository;
+    MediaInfoRepository mediaInfoRepository;
 
     @Autowired
-    private FileRepository fileRepository;
+    MediaRepository mediaRepository;
 
     @Autowired
-    private FileInfoRepository fileInfoRepository;
+    FileRepository fileRepository;
+
+    @Autowired
+    FileInfoRepository fileInfoRepository;
 
     @GetMapping("/test")
     public String test(){
@@ -38,22 +40,12 @@ public class MediaController {
 
     @GetMapping(value = "/mediaUploaded/{file}")
     public ResponseEntity<byte[]> testMediaUploaded(@PathVariable UUID file){
-        HttpHeaders headers = new HttpHeaders();
-        Media storedMedia = mediaRepository.findMediaById(file);
-        headers.setCacheControl(CacheControl.noCache().getHeaderValue()); // disabling caching for client who requests the resource
-        headers.setContentType(MediaType.parseMediaType(storedMedia.getType()));
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(storedMedia.getData(), headers, HttpStatus.OK);
-        return responseEntity;
+        return mediaServiceImplementation.testMediaUploaded(file);
     }
 
     @GetMapping(value = "/fileUploaded/{file}")
     public ResponseEntity<byte[]> testFileUploaded(@PathVariable UUID file){
-        HttpHeaders headers = new HttpHeaders();
-        File storedFile = fileRepository.findFileById(file);
-        headers.setCacheControl(CacheControl.noCache().getHeaderValue()); // disabling caching for client who requests the resource
-        headers.setContentType(MediaType.parseMediaType(storedFile.getType()));
-        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(storedFile.getData(), headers, HttpStatus.OK);
-        return responseEntity;
+        return mediaServiceImplementation.testFileUploaded(file);
     }
 
     @GetMapping(value = "/getUserMediaList/{id}")
@@ -78,56 +70,21 @@ public class MediaController {
 
     @PostMapping("/uploadMedia")
     public HttpStatus uploadMedia(@RequestParam("file") MultipartFile file, @RequestParam("userid") UUID userId, @RequestParam("adventureid") UUID adventureId){
-        try {
-            final byte[] content = file.getBytes();
-            Media uploadedMedia = new Media(UUID.randomUUID(), file.getContentType(), file.getName(), "DESCRIPTION", adventureId , userId);
-            MediaInfo uploadedMediaInfo = new MediaInfo(uploadedMedia.getId(), uploadedMedia.getType(), uploadedMedia.getName(), uploadedMedia.getDescription(), uploadedMedia.getAdventureID(), uploadedMedia.getOwner(),uploadedMedia.getPublicAccess());
-            uploadedMedia.setData(content);
-            mediaRepository.save(uploadedMedia);
-            mediaInfoRepository.save(uploadedMediaInfo);
-            return HttpStatus.OK;
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            return HttpStatus.NO_CONTENT;
-        }
-
+        return mediaServiceImplementation.uploadMedia(file,userId,adventureId);
     }
 
     @PostMapping("/uploadFile")
     public HttpStatus uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("userid") UUID userId, @RequestParam("adventureid") UUID adventureId){
-        try {
-            final byte[] content = file.getBytes();
-            File uploadedFile = new File(UUID.randomUUID(), file.getContentType(), file.getName(), "DESCRIPTION", adventureId , userId);
-            FileInfo uploadedFileInfo = new FileInfo(uploadedFile.getId(), uploadedFile.getType(), uploadedFile.getName(), uploadedFile.getDescription(), uploadedFile.getAdventureID(), uploadedFile.getOwner(),uploadedFile.getPublicAccess());
-            uploadedFile.setData(content);
-            fileRepository.save(uploadedFile);
-            fileInfoRepository.save(uploadedFileInfo);
-            return HttpStatus.OK;
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            return HttpStatus.NO_CONTENT;
-        }
+        return mediaServiceImplementation.uploadFile(file,userId,adventureId);
     }
 
     @GetMapping("/changeMediaAccess/{id}")
     public void changeMediaAccess(@PathVariable UUID id) {
-        Media media = mediaRepository.findMediaById(id);
-        MediaInfo mediaInfo = mediaInfoRepository.findMediaById(id);
-        media.setPublicAccess(!media.getPublicAccess());
-        mediaInfo.setPublicAccess(!mediaInfo.getPublicAccess());
-        mediaRepository.save(media);
-        mediaInfoRepository.save(mediaInfo);
+        mediaServiceImplementation.changeMediaAccess(id);
     }
 
     @GetMapping("/changeFileAccess/{id}")
     public void changeFileAccess(@PathVariable UUID id) {
-        File file = fileRepository.findFileById(id);
-        FileInfo fileInfo = fileInfoRepository.findFileInfoById(id);
-        file.setPublicAccess(!file.getPublicAccess());
-        fileInfo.setPublicAccess(!fileInfo.getPublicAccess());
-        fileRepository.save(file);
-        fileInfoRepository.save(fileInfo);
+        mediaServiceImplementation.changeFileAccess(id);
     }
 }
