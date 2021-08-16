cb207833a10a07353c448fab892fe67e1d039f86
kevichi <kevin9716cui@gmail.com>
2021-08-16 16:17:41 +0200

    feat(testcleanup):Commented out local tests for rework

--- backend/microservices/checklist-service/src/test/java/com/adventureit/checklist/LocalTests.java
+++ backend/microservices/checklist-service/src/test/java/com/adventureit/checklist/LocalTests.java
@@ -1,33 +1,33 @@
-package com.adventureit.checklist;
-
-import com.adventureit.checklist.Entity.ChecklistEntry;
-import com.adventureit.checklist.Repository.ChecklistEntryRepository;
-import com.adventureit.checklist.Repository.ChecklistRepository;
-import com.adventureit.checklist.Service.ChecklistServiceImplementation;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.UUID;
-
-@SpringBootTest
-public class LocalTests {
-    @Autowired
-    ChecklistRepository checklistRepository;
-
-    @Autowired
-    ChecklistEntryRepository checklistEntryRepository;
-
-    @Autowired
-    ChecklistServiceImplementation checklistServiceImplementation;
-
-    @Test
-    public void createEntry(){
-        checklistEntryRepository.save(new ChecklistEntry("Mock", UUID.randomUUID(), UUID.randomUUID()));
-    }
-
-    @Test
-    public void deleteEntry() throws Exception {
-        checklistServiceImplementation.removeChecklistEntry(UUID.fromString("ce0ae290-8366-490c-9865-f6206cf837a7"));
-    }
-}
+//package com.adventureit.checklist;
+//
+//import com.adventureit.checklist.Entity.ChecklistEntry;
+//import com.adventureit.checklist.Repository.ChecklistEntryRepository;
+//import com.adventureit.checklist.Repository.ChecklistRepository;
+//import com.adventureit.checklist.Service.ChecklistServiceImplementation;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.UUID;
+//
+//@SpringBootTest
+//public class LocalTests {
+//    @Autowired
+//    ChecklistRepository checklistRepository;
+//
+//    @Autowired
+//    ChecklistEntryRepository checklistEntryRepository;
+//
+//    @Autowired
+//    ChecklistServiceImplementation checklistServiceImplementation;
+//
+//    @Test
+//    public void createEntry(){
+//        checklistEntryRepository.save(new ChecklistEntry("Mock", UUID.randomUUID(), UUID.randomUUID()));
+//    }
+//
+//    @Test
+//    public void deleteEntry() throws Exception {
+//        checklistServiceImplementation.removeChecklistEntry(UUID.fromString("ce0ae290-8366-490c-9865-f6206cf837a7"));
+//    }
+//}
