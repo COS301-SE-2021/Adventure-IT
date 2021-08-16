cf654ad5b1e3c918f2d03d22229350e91e8d111f
Alex <alexwhuman@gmail.com>
2021-08-16 23:16:19 +0200

    fix(front-end): removed unused import

--- alex_flutter/adventure_it/lib/api/user_api.dart
+++ alex_flutter/adventure_it/lib/api/user_api.dart
@@ -6,7 +6,6 @@ import 'package:adventure_it/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:adventure_it/api/friendRequest.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
