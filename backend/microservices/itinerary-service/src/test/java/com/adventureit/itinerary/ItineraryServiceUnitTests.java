530dec34563100e43861a2c7900fc27866201e0a
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-08-18 02:33:47 +0200

    test: Itinerary Unit and Integration tests

--- backend/microservices/itinerary-service/src/test/java/com/adventureit/itinerary/ItineraryServiceUnitTests.java
+++ backend/microservices/itinerary-service/src/test/java/com/adventureit/itinerary/ItineraryServiceUnitTests.java
@@ -4,6 +4,7 @@ import com.adventureit.itinerary.Entity.Itinerary;
 import com.adventureit.itinerary.Entity.ItineraryEntry;
 import com.adventureit.itinerary.Repository.ItineraryEntryRepository;
 import com.adventureit.itinerary.Repository.ItineraryRepository;
+import com.adventureit.itinerary.Responses.ItineraryResponseDTO;
 import com.adventureit.itinerary.Service.ItineraryServiceImplementation;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +14,7 @@ import org.mockito.Mockito;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class ItineraryServiceUnitTests {
@@ -21,29 +23,28 @@ public class ItineraryServiceUnitTests {
     private final ItineraryServiceImplementation sut = new ItineraryServiceImplementation(mockItineraryRepository,mockItineraryEntryRepository);
 
     final UUID validItineraryID1 = UUID.randomUUID();
+    final UUID validAdventureID = UUID.randomUUID();
+    final UUID validUserID = UUID.randomUUID();
     final UUID validEntryID1 = UUID.randomUUID();
     final UUID validEntryID2 = UUID.randomUUID();
 
     ItineraryEntry mockEntry1 = new ItineraryEntry("Mock Entry 1","Mock Itinerary Entry",validEntryID1,validItineraryID1,UUID.randomUUID(), LocalDateTime.now());
     ItineraryEntry mockEntry2 = new ItineraryEntry("Mock Entry 2","Mock Itinerary Entry",validEntryID2,validItineraryID1,UUID.randomUUID(),LocalDateTime.now());
-    ArrayList<UUID> entries = new ArrayList<>(Arrays.asList(mockEntry1.getId(),mockEntry2.getId()));
-    Itinerary mockItinerary1 = new Itinerary("Mock Itinerary 1","Mock Itinerary",validItineraryID1,UUID.randomUUID(),UUID.randomUUID(),entries);
+    Itinerary mockItinerary1 = new Itinerary("Mock Itinerary 1","Mock Itinerary",validItineraryID1,validAdventureID,validUserID);
 
-//    @Test
-//    @Description("Ensuring a user can add an entry")
-//    public void addEntryValid_ReturnString() throws Exception {
-//        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
-//        Assertions.assertEquals(sut.addItineraryEntry("Mock", "Mock",validItineraryID1,"Location 1", LocalDateTime.now()),"Itinerary Entry successfully added");
-//    }
+    @Test
+    @Description("Ensure a user can create an itinerary")
+    public void createItineraryValid_ReturnString() throws Exception {
+        Assertions.assertEquals(sut.createItinerary("Mock","Mock",validAdventureID,validUserID),"Itinerary successfully created");
+    }
 
-//    @Test
-//    @Description("addItineraryEntry will throw an exception if a Itinerary entry with the same ID already exists")
-//    public void addEntryExistingID_ThrowException() throws Exception {
-//        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
-//        Assertions.assertThrows(Exception.class, ()->{
-//            sut.addItineraryEntry("Mock","Mock", validEntryID1,validItineraryID1, "Location 1", LocalDateTime.now());
-//        });
-//    }
+    @Test
+    @Description("Ensure a user can create an itinerary")
+    public void createItineraryNullField_ThrowException() throws Exception {
+        Assertions.assertThrows(Exception.class, ()->{
+            sut.createItinerary(null,"Mock",validAdventureID,validUserID);
+        });
+    }
 
     @Test
     @Description("addItineraryEntry will throw an exception if a field is null")
@@ -53,12 +54,13 @@ public class ItineraryServiceUnitTests {
         });
     }
 
-//    @Test
-//    @Description("Ensuring a user can remove an entry")
-//    public void removeEntryValid_ReturnString() throws Exception {
-//        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
-//        Assertions.assertEquals(sut.removeItineraryEntry(validEntryID1,validItineraryID1),"Itinerary Entry successfully removed");
-//    }
+    @Test
+    @Description("Ensuring a user can remove an entry")
+    public void removeEntryValid_ReturnString() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
+        Mockito.when(mockItineraryEntryRepository.findItineraryEntryById(validEntryID1)).thenReturn(mockEntry1);
+        Assertions.assertEquals(sut.removeItineraryEntry(validEntryID1),"Itinerary Entry successfully removed");
+    }
 
     @Test
     @Description("removeItineraryEntry will throw an exception if a Itinerary entry does not exist")
@@ -77,5 +79,58 @@ public class ItineraryServiceUnitTests {
         });
     }
 
+    @Test
+    @Description("Ensuring a user can softDelete an itinerary")
+    public void softDeleteItineraryValid() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryByIdAndDeleted(validItineraryID1,false)).thenReturn(mockItinerary1);
+        Assertions.assertEquals(sut.softDelete(validItineraryID1,validUserID),"Itinerary moved to bin");
+    }
 
+    @Test
+    @Description("Ensuring a only an owner can softDelete an itinerary")
+    public void softDeleteItineraryInvalid() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryByIdAndDeleted(validItineraryID1,false)).thenReturn(mockItinerary1);
+        Assertions.assertThrows(Exception.class, ()->{
+            sut.softDelete(validItineraryID1,UUID.randomUUID());
+        });
+    }
+
+    @Test
+    @Description("Ensuring a user can hardDelete a Itinerary")
+    public void hardDeleteItineraryValid() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryByIdAndDeleted(validItineraryID1,true)).thenReturn(mockItinerary1);
+        Assertions.assertEquals(sut.hardDelete(validItineraryID1,validUserID),"Itinerary deleted");
+    }
+
+    @Test
+    @Description("Ensuring a only an owner can hardDelete a Itinerary")
+    public void hardDeleteItineraryInvalid() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryByIdAndDeleted(validItineraryID1,true)).thenReturn(mockItinerary1);
+        Assertions.assertThrows(Exception.class, ()->{
+            sut.hardDelete(validItineraryID1,UUID.randomUUID());
+        });
+    }
+
+    @Test
+    @Description("Ensuring a user can view the trash")
+    public void viewTrashValid_ReturnsList() throws Exception {
+        mockItinerary1.setDeleted(true);
+        Mockito.when(mockItineraryRepository.findAllByAdventureID(validAdventureID)).thenReturn(List.of(mockItinerary1));
+        List<ItineraryResponseDTO> list = sut.viewTrash(validAdventureID);
+        Assertions.assertTrue(!list.isEmpty());
+    }
+
+    @Test
+    @Description("Ensuring a user can restore a Itinerary")
+    public void restoreItineraryValid_ReturnsString() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
+        Assertions.assertEquals(sut.restoreItinerary(validItineraryID1,validUserID),"Itinerary was restored");
+    }
+
+    @Test
+    @Description("Ensuring a user can get a Itinerary by ID")
+    public void getItineraryByIDValid_ReturnsString() throws Exception {
+        Mockito.when(mockItineraryRepository.findItineraryById(validItineraryID1)).thenReturn(mockItinerary1);
+        Assertions.assertTrue(sut.getItineraryById(validItineraryID1) != null);
+    }
 }
