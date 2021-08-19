23269557ddbbd02a9591073b215acf69dd154e24
ShivekG <55667611+ShivekG@users.noreply.github.com>
2021-08-19 23:57:55 +0200

    test: Improved budget integration tests

--- backend/microservices/budget-service/src/test/java/com/adventureit/budgetservice/BudgetServiceIntegrationTests.java
+++ backend/microservices/budget-service/src/test/java/com/adventureit/budgetservice/BudgetServiceIntegrationTests.java
@@ -3,9 +3,15 @@ import com.adventureit.budgetservice.Controllers.BudgetController;
 import com.adventureit.budgetservice.Entity.Budget;
 import com.adventureit.budgetservice.Entity.Category;
 import com.adventureit.budgetservice.Entity.UTOExpense;
+import com.adventureit.budgetservice.Entity.UTUExpense;
 import com.adventureit.budgetservice.Repository.BudgetEntryRepository;
 import com.adventureit.budgetservice.Repository.BudgetRepository;
+import com.adventureit.budgetservice.Requests.AddUTOExpenseEntryRequest;
+import com.adventureit.budgetservice.Requests.CreateBudgetRequest;
+import com.adventureit.budgetservice.Requests.EditBudgetRequest;
 import com.adventureit.budgetservice.Responses.BudgetResponseDTO;
+import com.adventureit.budgetservice.Responses.CreateBudgetResponse;
+import com.adventureit.budgetservice.Responses.EditBudgetResponse;
 import com.adventureit.budgetservice.Responses.ViewBudgetResponse;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
@@ -44,6 +50,72 @@ public class BudgetServiceIntegrationTests {
     @Description("Ensure that the controller is accepting traffic and responding")
     public void httpTest_returnResponse(){
         Assertions.assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/budget/test", String.class),"Budget Controller is functioning");
+    }
+
+    @Test
+    @Description("Ensure that the create function works")
+    public void httpCreateBudgetValid_returnResponse(){
+        CreateBudgetRequest request = new CreateBudgetRequest("Test Budget 1","Mock",UUID.randomUUID().toString(),UUID.randomUUID().toString());
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/create", request,String.class);
+        Assertions.assertEquals(response,"Budget was successfully created!" );
+    }
+
+    @Test
+    @Description("Create function will not work if field is null")
+    public void httpCreateBudgetInvalidNullField_returnResponse(){
+        CreateBudgetRequest request = new CreateBudgetRequest(null,"Mock",UUID.randomUUID().toString(),UUID.randomUUID().toString());
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/create", request,String.class);
+        Assertions.assertNotEquals(response,"Budget was successfully created!");
+    }
+
+    @Test
+    @Description("Ensure a user can add an entry to a budget")
+    public void httpAddEntryValid_returnResponse(){
+        UUID budgetID = UUID.randomUUID();
+        UUID adventureID = UUID.randomUUID();
+        Budget budget1 = new Budget(budgetID,"Test Budget 1","Mock",UUID.randomUUID(),adventureID);
+        budgetRepository.saveAndFlush(budget1);
+
+        AddUTOExpenseEntryRequest request = new AddUTOExpenseEntryRequest(budgetID,"User1",100,"Mock","Mock","Activities","Shop1");
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/addUTOExpense", request,String.class);
+        Assertions.assertEquals(response,"Entry added successfully!");
+    }
+
+    @Test
+    @Description("Add entry will not work if the budget does not exist")
+    public void httpAddEntryBudgetInvalid_returnResponse(){
+        AddUTOExpenseEntryRequest request = new AddUTOExpenseEntryRequest(UUID.randomUUID(),"User1",100,"Mock","Mock","Activities","Shop1");
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/addUTOExpense", request,String.class);
+        Assertions.assertNotEquals(response,"Entry added successfully!");
+    }
+
+    @Test
+    @Description("Add entry will not work if a field is null")
+    public void httpAddEntryInvalidNullField_returnResponse(){
+        UUID budgetID = UUID.randomUUID();
+        UUID adventureID = UUID.randomUUID();
+        Budget budget1 = new Budget(budgetID,"Test Budget 1","Mock",UUID.randomUUID(),adventureID);
+        budgetRepository.saveAndFlush(budget1);
+
+        AddUTOExpenseEntryRequest request = new AddUTOExpenseEntryRequest(budgetID,null,100,"Mock","Mock","Activities","Shop1");
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/addUTOExpense", request,String.class);
+        Assertions.assertEquals(response,"Entry added successfully!");
+    }
+
+    @Test
+    @Description("Ensure a user can edit an entry")
+    public void httpEditEntryValidNullField_returnResponse(){
+        UUID budgetID = UUID.randomUUID();
+        UUID entryID = UUID.randomUUID();
+        UUID adventureID = UUID.randomUUID();
+        Budget budget1 = new Budget(budgetID,"Test Budget 1","Mock",UUID.randomUUID(),adventureID);
+        UTUExpense expense = new UTUExpense(entryID,budgetID,100,"Mock","Mock",Category.Other,"User1","User2");
+        budgetRepository.saveAndFlush(budget1);
+        budgetEntryRepository.saveAndFlush(expense);
+
+        EditBudgetRequest request = new EditBudgetRequest(entryID,budgetID,100,"Mock","Mock",null,null);
+        String response = this.restTemplate.postForObject("http://localhost:" + port + "/budget/editBudget", request,String.class);
+        Assertions.assertEquals(response,"Budget successfully edited!");
     }
 
     @Test
