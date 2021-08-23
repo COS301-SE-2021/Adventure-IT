a5fba9676b138e80e25664f278cf44857afb9995
Alex <alexwhuman@gmail.com>
2021-08-23 21:11:16 +0200

    refactor(adventure-service): Removed unused constructor from GetAdventureByUUIDRequest

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/requests/GetAdventureByUUIDRequest.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/requests/GetAdventureByUUIDRequest.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 public class GetAdventureByUUIDRequest{
     private UUID id;
 
-    public GetAdventureByUUIDRequest(){}
-
     /**
      * This service will be used to generate a GetAdventureByUUID request
      * @param id id of the adventure that needs to be retrieved
