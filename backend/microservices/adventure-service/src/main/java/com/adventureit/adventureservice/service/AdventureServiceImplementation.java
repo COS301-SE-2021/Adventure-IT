b8a2c764cb61330385cf6a341203e0a9bb9eeba2
Alex <alexwhuman@gmail.com>
2021-08-23 21:55:53 +0200

    refactor(adventure-service): Removed unused import in AdventureServiceImplementation

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/service/AdventureServiceImplementation.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/service/AdventureServiceImplementation.java
@@ -8,7 +8,6 @@ import com.adventureit.adventureservice.requests.CreateAdventureRequest;
 import com.adventureit.adventureservice.requests.GetAdventureByUUIDRequest;
 import com.adventureit.adventureservice.responses.*;
 import com.adventureit.adventureservice.exceptions.NullFieldException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
