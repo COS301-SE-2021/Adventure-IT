061457f79236c22a76dba96f454c4cc660a581e0
kevichi <kevin9716cui@gmail.com>
2021-08-24 08:35:30 +0200

    feat(clean-up) Cleaning up code for User Service

--- backend/microservices/user-service/src/main/java/com/adventureit/userservice/UserServiceApplication.java
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/UserServiceApplication.java
@@ -1,10 +1,7 @@
 package com.adventureit.userservice;
 
-import com.adventureit.userservice.Service.UserServiceImplementation;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
---
+++ backend/microservices/user-service/src/main/java/com/adventureit/userservice/exceptions/InvalidUserPhoneNumberException.java
@@ -0,0 +1,12 @@
+package com.adventureit.userservice.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid User phone number")
+public class InvalidUserPhoneNumberException extends RuntimeException {
+    public InvalidUserPhoneNumberException(String message) {
+        super(message);
+    }
+}
