cc49bd310fec3bfe15e48aa1c0a63d07fa34cea1
kevichi <kevin9716cui@gmail.com>
2021-08-27 16:05:29 +0200

    refactor(debug): Debugged main controller

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/responses/GetAdventureByUUIDResponse.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/responses/GetAdventureByUUIDResponse.java
@@ -3,9 +3,11 @@ package com.adventureit.adventureservice.responses;
 import com.adventureit.adventureservice.entity.Adventure;
 
 public class GetAdventureByUUIDResponse {
-    private final boolean success;
+    private boolean success;
     private Adventure adventure;
 
+
+    public GetAdventureByUUIDResponse(){}
     /**
      * This object will store the response attributes from the GetAdventureByUUID service, currently a mock
      * adventure will be sent back for testing purposes but for future implementation an adventure will be found from the database
@@ -13,6 +15,7 @@ public class GetAdventureByUUIDResponse {
      * @param success success attribute to indicate whether the service was successful
      * @param adventure adventure to be returned
      */
+
 
     public GetAdventureByUUIDResponse(boolean success, Adventure adventure){
         this.adventure = adventure;
