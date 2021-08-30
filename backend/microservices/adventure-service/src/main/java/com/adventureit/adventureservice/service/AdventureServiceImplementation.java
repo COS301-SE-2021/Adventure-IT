0f6be96830e6ab78bc1a14f92158b37c7f28eaaa
Alex <alexwhuman@gmail.com>
2021-08-30 23:48:03 +0200

    feat(deployment): Docker integration
    - Created dockerfiles for each system component
    - Used environment variables throughout backend
    - Created a docker-compose.yml to spin up all images for a local installation

--- backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/service/AdventureServiceImplementation.java
+++ backend/microservices/adventure-service/src/main/java/com/adventureit/adventureservice/service/AdventureServiceImplementation.java
@@ -125,7 +125,6 @@ public class AdventureServiceImplementation implements AdventureService {
     @Override
     public List<GetAdventuresByUserUUIDResponse> getAllAdventuresByUUID(UUID id) {
         List<Adventure> userAdventures = adventureRepository.findAllByOwnerIdOrAttendeesContains(id,id);
-
         return sortAdventures(userAdventures);
     }
 
