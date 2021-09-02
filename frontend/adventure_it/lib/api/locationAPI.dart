aa5afe8e775242f7fdf69af42ad540d03b3075ec
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-09-02 18:35:40 +0200

    Created the rotating profile pics for adventure page and created currentLocation entity

--- frontend/adventure_it/lib/api/locationAPI.dart
+++ frontend/adventure_it/lib/api/locationAPI.dart
@@ -2,13 +2,16 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:core';
 import 'package:adventure_it/api/placeSearch.dart';
+import 'package:adventure_it/api/userAPI.dart';
 import 'package:adventure_it/constants.dart';
 import 'package:http/http.dart' as http;
+
+import 'adventure.dart';
+import 'currentLocation.dart';
 
 class LocationApi {
   static Future<List<PlaceSearch>> getSuggestions(String query) async {
     http.Response response = await _getSuggestions(query);
-
 
     if (response.statusCode != 200) {
       throw Exception('Failed to load place suggestions: ${response.body}');
@@ -25,5 +28,46 @@ class LocationApi {
   static Future<http.Response> _getSuggestions(query) async {
     return http.get(Uri.parse(
         'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$query&key=$googleMapsKey'));
+  }
+
+  static Future<List<CurrentLocation>> getAllCurrentLocations(
+      Adventure a) async {
+    http.Response response = await _getAllCurrentLocations(a.adventureId);
+
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Failed to load current locations for adventure: ${response.body}');
+    }
+
+    List<CurrentLocation> locations = (jsonDecode(response.body) as List)
+        .map((x) => CurrentLocation.fromJson(x))
+        .toList();
+
+    return locations;
+  }
+
+  static Future<http.Response> _getAllCurrentLocations(
+      String adventureID) async {
+    return http.get(
+        Uri.parse(mainApi + "/location/getAllCurrentLocations/" + adventureID));
+  }
+
+  static Future setCurrentLocation(String latitude, String longitude) async {
+    http.Response response = await _setCurrentLocation(latitude, longitude);
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to set location for user: ${response.body}');
+    }
+  }
+
+  static Future<http.Response> _setCurrentLocation(
+      String latitude, String longitude) async {
+    return http.get(Uri.parse(mainApi +
+        "/storeCurrentLocation/{userID}/{latitude}/{longitude}" +
+        UserApi.getInstance().getUserProfile()!.userID +
+        "/" +
+        latitude +
+        "/" +
+        longitude));
   }
 }
--- frontend/adventure_it/lib/frontEnd/AdventureAttendees.dart
+++ frontend/adventure_it/lib/frontEnd/AdventureAttendees.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:adventure_it/Providers/adventure_model.dart';
@@ -17,30 +18,54 @@ class AdventureAttendees extends StatelessWidget {
     this.currentAdventure = a;
   }
 
+  CarouselController buttonCarouselController = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
         create: (context) => AdventureAttendeesModel(currentAdventure!),
-        builder: (context, widget) =>
-            Scaffold(
-                drawer: NavDrawer(),
-                backgroundColor: Theme
-                    .of(context)
-                    .scaffoldBackgroundColor,
-                appBar: AppBar(
-                    title: Center(
-                        child: Text("Adventurers",
-                            style: new TextStyle(
-                                color: Theme
-                                    .of(context)
-                                    .textTheme
-                                    .bodyText1!
-                                    .color))),
-                    backgroundColor: Theme
-                        .of(context)
-                        .primaryColorDark),
-                body: Center(
-                    child: Container())));
+        builder: (context, widget) => Scaffold(
+            drawer: NavDrawer(),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            appBar: AppBar(
+                title: Center(
+                    child: Text("Adventurers",
+                        style: new TextStyle(
+                            color:
+                                Theme.of(context).textTheme.bodyText1!.color))),
+                backgroundColor: Theme.of(context).primaryColorDark),
+            body: Consumer<AdventureAttendeesModel>(
+                builder: (context, attendeeModel, child) {
+              return Column(children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 10,
+                ),
+                CarouselSlider(
+                  items: attendeeModel.attendees!.map((i) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage("logo.png"),
+                        )
+                      )
+                    );
+                  }).toList(),
+                  carouselController: buttonCarouselController,
+                  options: CarouselOptions(
+                    autoPlay: false,
+                    enlargeCenterPage: true,
+                    viewportFraction: 0.9,
+                    aspectRatio: 2.0,
+                    initialPage: 2,
+                    enableInfiniteScroll: true,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 10,
+                ),
+              ]);
+            })));
   }
 }
 //new Tab(icon: new Image.asset(choice.icon.image), text: choice.title),
