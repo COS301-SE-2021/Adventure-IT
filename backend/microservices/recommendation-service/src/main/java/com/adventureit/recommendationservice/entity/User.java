b8fa17fc839776a08c2f7e16a19ad9524abc081a
Alex <alexwhuman@gmail.com>
2021-09-02 21:08:39 +0200

    feat(recommendation-service): implemented User and Location entities

--- backend/microservices/recommendation-service/src/main/java/com/adventureit/recommendationservice/entity/User.java
+++ backend/microservices/recommendation-service/src/main/java/com/adventureit/recommendationservice/entity/User.java
@@ -1,4 +1,26 @@
 package com.adventureit.recommendationservice.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
 public class User {
+    @Id
+    private final UUID userId;
+    @ManyToMany
+    private List<Location> likedLocations;
+    @ManyToMany
+    private List<Location> visitedLocations;
+
+    public User(){
+        this.userId = UUID.randomUUID();
+    }
+
+    public User(UUID id){
+        this.userId = id;
+    }
+
 }
