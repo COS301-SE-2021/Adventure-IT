a05d44d6ac05f2fa5727ec5fabd44bec413e3369
kevichi <kevin9716cui@gmail.com>
2021-09-16 16:57:42 +0200

    feat(Main-controller):Added circuit breaker functionality to notification reroute controller

--- backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerNotificationReroute.java
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerNotificationReroute.java
@@ -1,10 +1,12 @@
 package com.adventureit.maincontroller.controller;
 
 
+import com.adventureit.maincontroller.service.MainControllerServiceImplementation;
 import com.adventureit.shareddtos.notification.NotificationDTO;
 import com.adventureit.shareddtos.notification.requests.CreateNotificationRequest;
 import com.adventureit.shareddtos.notification.requests.RetrieveNotificationRequest;
 import com.adventureit.shareddtos.notification.requests.SendEmailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,26 +16,36 @@ import java.util.List;
 @RequestMapping("/notification")
 public class MainControllerNotificationReroute {
 
-
     private final RestTemplate restTemplate = new RestTemplate();
+    private MainControllerServiceImplementation service;
 
-    String IP = "http://localhost";
-    String notificationPort = "9004";
+    private final String IP = "http://localhost";
+    private final String notificationPort = "9004";
+
+    @Autowired
+    public MainControllerNotificationReroute(MainControllerServiceImplementation service) {
+        this.service = service;
+    }
 
     @PostMapping("/sendemail")
-    public String sendEmail(@RequestBody SendEmailRequest req){
+    public String sendEmail(@RequestBody SendEmailRequest req) throws Exception {
+        String[] ports = {notificationPort};
+        service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(IP + ":" + notificationPort + "/notification/sendemail/", req, String.class);
 
     }
 
     @PostMapping("/createNotification")
-    public String createNotification(@RequestBody CreateNotificationRequest req){
+    public String createNotification(@RequestBody CreateNotificationRequest req) throws Exception {
+        String[] ports = {notificationPort};
+        service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(IP + ":" + notificationPort + "/notification/createNotification/", req, String.class);
-
     }
 
     @PostMapping("/retrieveNotification")
-    public List<NotificationDTO> test3(@RequestBody RetrieveNotificationRequest req){
+    public List<NotificationDTO> test3(@RequestBody RetrieveNotificationRequest req) throws Exception {
+        String[] ports = {notificationPort};
+        service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(IP + ":" + notificationPort + "/notification/retrieveNotification/", req, List.class);
     }
 
