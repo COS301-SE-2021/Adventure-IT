bff897e47c30e0356a7286513a1d79255ce8d8c8
kevichi <kevin9716cui@gmail.com>
2021-09-16 16:57:05 +0200

    feat(Main-controller):Added circuit breaker functionality to timeline reroute controller

--- backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerTimelineReroute.java
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerTimelineReroute.java
@@ -1,7 +1,9 @@
 package com.adventureit.maincontroller.controller;
 
 
+import com.adventureit.maincontroller.service.MainControllerServiceImplementation;
 import com.adventureit.shareddtos.timeline.responses.TimelineDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +17,29 @@ import java.util.UUID;
 @RequestMapping("/timeline")
 public class MainControllerTimelineReroute {
 
-
     private final RestTemplate restTemplate = new RestTemplate();
+    private MainControllerServiceImplementation service;
 
     private final String IP = "http://localhost";
     private final String timelinePort = "9012";
 
+    @Autowired
+    public MainControllerTimelineReroute(MainControllerServiceImplementation service) {
+        this.service = service;
+    }
+
     @GetMapping("/getTimelineByAdventure/{id}")
-    public List<TimelineDTO> getTimelineByAdventureID(@PathVariable UUID id){
+    public List<TimelineDTO> getTimelineByAdventureID(@PathVariable UUID id) throws Exception {
+        String[] ports = {timelinePort};
+        service.pingCheck(ports,restTemplate);
         return restTemplate.getForObject(IP + ":" + timelinePort + "/timeline/getTimelineByAdventure/"+id, List.class);
 
     }
 
     @GetMapping("/deleteTimelineByAdventureID/{id}")
-    public String deleteTimelineByAdventureID(@PathVariable UUID id){
+    public String deleteTimelineByAdventureID(@PathVariable UUID id) throws Exception {
+        String[] ports = {timelinePort};
+        service.pingCheck(ports,restTemplate);
         return restTemplate.getForObject(IP + ":" + timelinePort + "/timeline/deleteTimelineByAdventureID/"+id, String.class);
 
     }
