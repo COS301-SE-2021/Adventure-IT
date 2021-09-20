fdb06ca907fd42f20e858f74f93425e4bd861e5f
Simran <simrathilal@gmail.com>
2021-09-20 04:04:14 +0200

    fixed exception handling in notification reroute

--- backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerNotificationReroute.java
+++ backend/main-controller/src/main/java/com/adventureit/maincontroller/controller/MainControllerNotificationReroute.java
@@ -1,6 +1,7 @@
 package com.adventureit.maincontroller.controller;
 
 
+import com.adventureit.maincontroller.exceptions.ControllerNotAvailable;
 import com.adventureit.maincontroller.service.MainControllerServiceImplementation;
 import com.adventureit.shareddtos.notification.NotificationDTO;
 import com.adventureit.shareddtos.notification.requests.*;
@@ -26,7 +27,7 @@ public class MainControllerNotificationReroute {
     }
 
     @PostMapping("/sendemail")
-    public String sendEmail(@RequestBody SendEmailRequest req) throws Exception {
+    public String sendEmail(@RequestBody SendEmailRequest req) throws ControllerNotAvailable, InterruptedException {
         String[] ports = {NOTIFICATION_PORT};
         service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(INTERNET_PORT + ":" + NOTIFICATION_PORT + "/notification/sendemail/", req, String.class);
@@ -34,14 +35,14 @@ public class MainControllerNotificationReroute {
     }
 
     @PostMapping("/createNotification")
-    public String createNotification(@RequestBody CreateNotificationRequest req) throws Exception {
+    public String createNotification(@RequestBody CreateNotificationRequest req) throws ControllerNotAvailable, InterruptedException {
         String[] ports = {NOTIFICATION_PORT};
         service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(INTERNET_PORT + ":" + NOTIFICATION_PORT + "/notification/createNotification/", req, String.class);
     }
 
     @PostMapping("/retrieveNotification")
-    public List<NotificationDTO> test3(@RequestBody RetrieveNotificationRequest req) throws Exception {
+    public List<NotificationDTO> test3(@RequestBody RetrieveNotificationRequest req) throws ControllerNotAvailable, InterruptedException {
         String[] ports = {NOTIFICATION_PORT};
         service.pingCheck(ports,restTemplate);
         return restTemplate.postForObject(INTERNET_PORT + ":" + NOTIFICATION_PORT + "/notification/retrieveNotification/", req, List.class);
