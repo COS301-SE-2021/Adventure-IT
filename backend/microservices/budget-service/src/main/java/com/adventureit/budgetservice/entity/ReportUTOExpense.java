f97a9cf8a70006572af77fd5bbdd79a9c6a0f19f
kevichi <kevin9716cui@gmail.com>
2021-09-20 17:37:16 +0200

    feat(main controller-service): Created new entities for graph algorithm

---
+++ backend/microservices/budget-service/src/main/java/com/adventureit/budgetservice/entity/ReportUTOExpense.java
@@ -0,0 +1,41 @@
+package com.adventureit.budgetservice.entity;
+
+import com.adventureit.shareddtos.budget.Category;
+
+import java.util.UUID;
+
+public class ReportUTOExpense extends ReportBudgetEntity{
+    private String payee;
+
+    /**
+     * Expense model Constructor which takes in the following parameters:
+     * @param id Expense id
+     * @param amount amount used in specific entry
+     * @param title title of entry
+     * @param description short description of entry
+     */
+    public ReportUTOExpense(UUID id, UUID entryContainerID, double amount, String title, String description, Category category, String payer, String payee){
+        super(id,entryContainerID,amount,title,description, category, payer);
+        this.payee = payee;
+    }
+
+    public ReportUTOExpense(UUID entryContainerID, double amount, String title, String description, Category category, String payer, String payee){
+        super(entryContainerID,amount,title,description, category, payer);
+        this.payee = payee;
+    }
+
+    /**
+     * Default Constructor
+     */
+    public ReportUTOExpense() {
+
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public void setPayee(String payee) {
+        this.payee = payee;
+    }
+}
