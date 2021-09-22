e5997ff49d39bcd91d3cc34daafa8def0a3bbdc6
Alex <alexwhuman@gmail.com>
2021-09-22 14:04:11 +0200

    fix(front-end): Updated API endpoint constants to match production proxy configuration

--- frontend/adventure_it/lib/constants.dart
+++ frontend/adventure_it/lib/constants.dart
@@ -2,14 +2,14 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
 final authApiGetToken = kIsWeb
-    ? "http://3.142.172.27:5001/auth/realms/adventure-it/protocol/openid-connect/token"
-    : "http://3.142.172.27:5001/auth/realms/adventure-it/protocol/openid-connect/token";
+    ? "http://app.adventure-it.co.za/keycloak/auth/realms/adventure-it/protocol/openid-connect/token"
+    : "http://app.adventure-it.co.za/keycloak/auth/realms/adventure-it/protocol/openid-connect/token";
 final authApiAdmin = kIsWeb
-    ? "http://3.142.172.27:5001/auth/admin/realms/adventure-it/"
-    : "http://3.142.172.27:5001/auth/admin/realms/adventure-it/";
+    ? "http://app.adventure-it.co.za/keycloak/auth/admin/realms/adventure-it/"
+    : "http://app.adventure-it.co.za/keycloak/auth/admin/realms/adventure-it/";
 final mainApi = kIsWeb
-    ? "http://main-controller-235091144.us-east-2.elb.amazonaws.com"
-    : "http://main-controller-235091144.us-east-2.elb.amazonaws.com";
+    ? "http://app.adventure-it.co.za/api/"
+    : "http://app.adventure-it.co.za/api/";
 final googleMapsKey = "AIzaSyD8xsVljufOFTmpnVZI2KzobIdAvKjWdTE";
 
 final darkTheme = ThemeData(
