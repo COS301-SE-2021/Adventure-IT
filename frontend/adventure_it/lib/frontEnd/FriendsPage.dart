b3b75f822f49fa80be9a6aff95ce2023b9ed73af
A Flowerday <59618430+AFlowerday@users.noreply.github.com>
2021-09-28 18:19:33 +0200

    updated asset images with correct path

--- frontend/adventure_it/lib/frontEnd/FriendsPage.dart
+++ frontend/adventure_it/lib/frontEnd/FriendsPage.dart
@@ -205,7 +205,7 @@ class GetFriends extends StatelessWidget {
                                                         shape: BoxShape.circle,
                                                         image: DecorationImage(
                                                             fit: BoxFit.fitWidth,
-                                                            image: AssetImage("pfp.png")
+                                                            image: AssetImage("assets/pfp.png")
                                                         ))),
 
                                                 errorWidget: (context, url, error) => Container(
@@ -219,7 +219,7 @@ class GetFriends extends StatelessWidget {
                                                         shape: BoxShape.circle,
                                                         image: DecorationImage(
                                                             fit: BoxFit.fitWidth,
-                                                            image: AssetImage("pfp.png")
+                                                            image: AssetImage("assets/pfp.png")
                                                         )))),
                                             title: Text(
                                                 friendModel.friends!
@@ -439,7 +439,7 @@ class GetFriendRequests extends StatelessWidget {
                                                   shape: BoxShape.circle,
                                                   image: DecorationImage(
                                                       fit: BoxFit.fill,
-                                                      image: AssetImage("pfp.png")
+                                                      image: AssetImage("assets/pfp.png")
                                                   ))),
 
                                           errorWidget: (context, url, error) => Container(
@@ -453,7 +453,7 @@ class GetFriendRequests extends StatelessWidget {
                                                   shape: BoxShape.circle,
                                                   image: DecorationImage(
                                                       fit: BoxFit.fill,
-                                                      image: AssetImage("pfp.png")
+                                                      image: AssetImage("assets/pfp.png")
                                                   )))),
                                       title: Text(
                                           friendModel.friends!
