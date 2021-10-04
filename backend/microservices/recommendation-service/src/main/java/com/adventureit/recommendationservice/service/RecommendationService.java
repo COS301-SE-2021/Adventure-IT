d2e1cc27ade12ca3a8027b37dbaeab0adb19365f
Alex <alexwhuman@gmail.com>
2021-10-04 12:33:24 +0200

    fix(recommendation-service): corrected recommendation filtering

--- backend/microservices/recommendation-service/src/main/java/com/adventureit/recommendationservice/service/RecommendationService.java
+++ backend/microservices/recommendation-service/src/main/java/com/adventureit/recommendationservice/service/RecommendationService.java
@@ -95,7 +95,7 @@ public class RecommendationService {
         List<RecommendedLocation> filteredLocations = new ArrayList<>();
 
         for(RecommendedLocation l : locations){
-            if(locationFilter.contains(l.getLocationString())){
+            if(l.getLocationString().contains(locationFilter)){
                 filteredLocations.add(l);
             }
         }
@@ -210,7 +210,7 @@ public class RecommendationService {
             numRecommendations = recommendedLocations.size();
         }
 
-        String[][] returnMatrix = new String[numLocations][2];
+        String[][] returnMatrix = new String[numRecommendations][2];
         for(int i = 0; i<numRecommendations;i++){
             RecommendedLocation currentLocation = recommendedLocationRepository.findLocationByLocationId(recommendedLocations.get(i));
             returnMatrix[i][0] = recommendedLocations.get(i).toString();
@@ -228,7 +228,7 @@ public class RecommendationService {
         int numPopular=Integer.parseInt(numPop);
         locations.sort(Comparator.comparing(RecommendedLocation::getVisits));
         for(RecommendedLocation location : locations){
-            if(locationFilter.contains(location.getLocationString())){
+            if(location.getLocationString().contains(locationFilter)){
                 filteredLocations.add(location);
             }
         }
@@ -239,17 +239,18 @@ public class RecommendationService {
             locations = filteredLocations;
         }
 
-        String[][] returnMatrix = new String[locations.size()][2];
         List<RecommendedUser> users = this.recommendedUserRepository.findAll();
+
 
         if(locations.isEmpty())
         {
-            return returnMatrix;
+            return null;
         } else if(locations.size()<numPopular)
         {
             numPopular=locations.size();
         }
 
+        String[][] returnMatrix = new String[numPopular][2];
 
         // Find index of current user:
         int userIndex = -1;
